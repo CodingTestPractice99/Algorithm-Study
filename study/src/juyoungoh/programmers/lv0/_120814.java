50d446015156087275ee30dc48d7f5756251efe5
ohjuyoungg <ejrm12@naver.com>
2024-02-16 21:31:59 +0900

    feat: 프로그래머스 피자 나눠 먹기(1)

--- study/src/juyoungoh/programmers/lv0/_120814.java
+++ study/src/juyoungoh/programmers/lv0/_120814.java
@@ -1,2 +1,26 @@
-package juyoungoh.programmers.lv0;public class _120814 {
+package juyoungoh.programmers.lv0;
+
+public class _120814 {
+
+    public static int solution(int n) {
+        int answer = 1;
+        int piece = 7;
+
+        if (piece / n >= 1) {
+            return 1;
+        } else {
+            while (piece / n < 1) {
+                piece += 7;
+                answer++;
+            }
+            return answer;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(solution(7)); // 1
+        System.out.println(solution(1)); // 1
+        System.out.println(solution(15)); // 3
+    }
 }
