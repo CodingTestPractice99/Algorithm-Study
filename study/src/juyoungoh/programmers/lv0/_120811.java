4e24cfc8550fc5b9e96920ddfa0a3d3dc3fc5c20
ohjuyoungg <ejrm12@naver.com>
2024-02-22 23:50:14 +0900

    test

---
+++ study/src/juyoungoh/programmers/lv0/_120811.java
@@ -0,0 +1,13 @@
+package juyoungoh.programmers.lv0;
+
+public class _120811 {
+    public static int solution(int[] array) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        solution(new int[]{1, 2, 7, 10, 11});
+        solution(new int[]{9, -1, 0});
+    }
+}
