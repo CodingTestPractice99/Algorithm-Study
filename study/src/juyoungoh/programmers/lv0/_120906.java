d903ab80a02ac1c011ab9a42da722cee34a217c7
ohjuyoungg <ejrm12@naver.com>
2024-02-24 14:26:08 +0900

    프로그래머스: 자릿수 더하기(240224)

---
+++ study/src/juyoungoh/programmers/lv0/_120906.java
@@ -0,0 +1,22 @@
+package juyoungoh.programmers.lv0;
+
+import java.util.Arrays;
+
+public class _120906 {
+
+    public static int solution(int n) {
+        int answer = 0;
+        String str = String.valueOf(n); // "1234"
+        String array[] = str.split("");
+
+        for (int i = 0; i < array.length; i++) {
+            answer += Integer.parseInt(array[i]);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(1234));
+        System.out.println(solution(930211));
+    }
+}
