bea9eff4c4342c6649f9e32eed85f554241e9264
ohjuyoungg <ejrm12@naver.com>
2024-02-25 17:21:51 +0900

    백준: A+B - 8 11022번 (240225)

---
+++ study/src/juyoungoh/beakjoon/_11022.java
@@ -0,0 +1,30 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _11022 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st;
+
+        int inputNumber = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= inputNumber; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append("Case #").append(i + ": ").append(a + " + " + b).append(" = ").append(a+b).append("\n");
+        }
+        System.out.println(sb);
+
+
+    }
+}
