f9cb03172228f773f1d8e192aa09d09162d208e3
ohjuyoungg <ejrm12@naver.com>
2024-02-26 18:30:16 +0900

    백준: 빠른 A+B 15552번 (240226)

---
+++ study/src/juyoungoh/beakjoon/_15552.java
@@ -0,0 +1,27 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class _15552 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int inputNum = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= inputNum; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append(a+b).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
