4081b9f2844e058fb4e84dc1d0bd3fec617c0580
ohjuyoungg <ejrm12@naver.com>
2024-03-02 20:19:51 +0900

    백준: 별 찍기 - 2 2439번 (240302)

--- study/src/juyoungoh/beakjoon/_2439.java
+++ study/src/juyoungoh/beakjoon/_2439.java
@@ -1,2 +1,29 @@
-package juyoungoh.beakjoon;public class _2439 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2439 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        String star = "*";
+        String blank = " ";
+
+        int n = Integer.parseInt(br.readLine()); // 5
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                sb.append(blank);
+            }
+            for (int k = 1; k <= i; k++) {
+                sb.append(star);
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
 }
