7c5a9033ecab5595235f0a360e2ac369044007e1
ohjuyoungg <ejrm12@naver.com>
2024-03-02 18:59:37 +0900

    프로그래머스: 120802번 (240229)

---
+++ study/src/juyoungoh/programmers/lv0/_120802.java
@@ -0,0 +1,13 @@
+package juyoungoh.programmers.lv0;
+
+public class _120802 {
+    public int solution(int num1, int num2) {
+        int answer = num1 + num2;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        _120802 solution = new _120802();
+        System.out.println(solution.solution(1, 2));
+    }
+}
