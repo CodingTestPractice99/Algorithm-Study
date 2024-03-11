7148f271ae3ecd45ca3e92be22d2b942722fda8f
ohjuyoungg <ejrm12@naver.com>
2024-03-11 22:50:39 +0900

    프로그래머스: 중앙값 구하기 (240311)

--- study/src/juyoungoh/programmers/lv0/_120811.java
+++ study/src/juyoungoh/programmers/lv0/_120811.java
@@ -1,13 +1,26 @@
 package juyoungoh.programmers.lv0;
 
+import java.util.Arrays;
+
 public class _120811 {
     public static int solution(int[] array) {
         int answer = 0;
+        Arrays.sort(array);
+
+        if (array.length == 1) {
+            return array[0];
+        }
+
+        int length = array.length / 2;
+        for (int i = 0; i < array.length; i++) {
+         answer = array[length];
+        }
         return answer;
     }
 
     public static void main(String[] args) {
-        solution(new int[]{1, 2, 7, 10, 11});
-        solution(new int[]{9, -1, 0});
+        System.out.println(solution(new int[]{1, 2, 7, 10, 11}));
+        System.out.println(solution(new int[]{9, -1, 0}));
+        System.out.println(solution(new int[]{0}));
     }
 }
