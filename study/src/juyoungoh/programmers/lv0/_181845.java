f0fb7f60ca29f65b2507bee796fb14458cbfbf87
ohjuyoungg <ejrm12@naver.com>
2024-03-16 19:24:08 +0900

    프로그래머스: 문자열로 변환 (240316)

---
+++ study/src/juyoungoh/programmers/lv0/_181845.java
@@ -0,0 +1,14 @@
+package juyoungoh.programmers.lv0;
+
+public class _181845 {
+    public static String solution(int n) {
+        String answer = "";
+        answer =  String.valueOf(n);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(123));
+        System.out.println(solution(2573));
+    }
+}
