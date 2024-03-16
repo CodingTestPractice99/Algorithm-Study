45a7d7ebe0472a57bb1ba048284572a76ffebe1d
ohjuyoungg <ejrm12@naver.com>
2024-03-16 19:20:04 +0900

    프로그래머스: 문자열 붙여서 출력하기 (240316)

---
+++ study/src/juyoungoh/programmers/lv0/_181946.java
@@ -0,0 +1,13 @@
+package juyoungoh.programmers.lv0;
+
+import java.util.Scanner;
+
+public class _181946 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(a+b);
+    }
+}
