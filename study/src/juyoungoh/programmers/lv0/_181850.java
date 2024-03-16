707adb5b6269301f17af90b7e944de3c2f6d2c77
ohjuyoungg <ejrm12@naver.com>
2024-03-16 19:51:47 +0900

    프로그래머스: 정수 부분 (240316)

---
+++ study/src/juyoungoh/programmers/lv0/_181850.java
@@ -0,0 +1,13 @@
+package juyoungoh.programmers.lv0;
+
+public class _181850 {
+    public static int solution(double flo) {
+        int answer = (int) flo;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(1.42));
+        System.out.println(solution(69.32));
+    }
+}
