3da4963b36f8ce73ce36f7a16f3da02e2b0f374d
ohjuyoungg <ejrm12@naver.com>
2024-03-17 18:04:45 +0900

    프로그래머스: 문자열을 정수로 변환하기 (240317)

---
+++ study/src/juyoungoh/programmers/lv0/_181848.java
@@ -0,0 +1,15 @@
+package juyoungoh.programmers.lv0;
+
+public class _181848 {
+
+    public static int solution(String n_str) {
+        int answer;
+        answer = Integer.parseInt(n_str);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("10"));
+        System.out.println(solution("8542"));
+    }
+}
