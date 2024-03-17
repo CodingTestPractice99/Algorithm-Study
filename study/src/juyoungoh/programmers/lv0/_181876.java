25a2df92077d1a823706b6f187515a77e1e8c2e2
ohjuyoungg <ejrm12@naver.com>
2024-03-17 18:10:20 +0900

    프로그래머스: 소문자로 바꾸기 (240317)

---
+++ study/src/juyoungoh/programmers/lv0/_181876.java
@@ -0,0 +1,15 @@
+package juyoungoh.programmers.lv0;
+
+public class _181876 {
+
+    public static String solution(String myString) {
+        String answer;
+        answer = myString.toLowerCase();
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("aBcDeFg"));
+        System.out.println(solution("aaa"));
+    }
+}
