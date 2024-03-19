526e536ed16517645bdd1b7f0068e954801f1536
ohjuyoungg <ejrm12@naver.com>
2024-03-19 22:59:55 +0900

    프로그래머스: 대문자로 바꾸기 (240319)

---
+++ study/src/juyoungoh/programmers/lv0/_181877.java
@@ -0,0 +1,15 @@
+package juyoungoh.programmers.lv0;
+
+public class _181877 {
+
+    public static String solution(String myString) {
+        String answer = "";
+        answer = myString.toUpperCase();
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("aBcDeFg"));
+        System.out.println(solution("AAA"));
+    }
+}
