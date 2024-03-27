7fc3d12b252c678869d82b9b4ef7198442475196
ohjuyoungg <ejrm12@naver.com>
2024-03-27 17:57:13 +0900

    프로그래머스: 뒤에서 5등 위로 (240327)

--- study/src/juyoungoh/programmers/lv0/_181852.java
+++ study/src/juyoungoh/programmers/lv0/_181852.java
@@ -1,2 +1,21 @@
-package juyoungoh.programmers.lv0;public class _181852 {
+package juyoungoh.programmers.lv0;
+
+import java.util.Arrays;
+
+public class _181852 {
+    public static int[] solution(int[] num_list) {
+        int[] sortArrays = Arrays.stream(num_list).sorted().toArray();
+        int[] answer = new int[num_list.length - 5];
+
+        int count = 0;
+        for (int i = 5; i < sortArrays.length; i++) {
+            answer[count] += sortArrays[i];
+            count++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[]{12, 4, 15, 46, 38, 1, 14, 56, 32, 10})));
+    }
 }
