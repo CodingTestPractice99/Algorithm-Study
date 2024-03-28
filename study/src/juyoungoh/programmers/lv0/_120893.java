0adc366e5500e71fa951a565bc9c2b623130897c
ohjuyoungg <ejrm12@naver.com>
2024-03-28 22:53:16 +0900

    프로그래머스: 대문자와 소문자 (240328)

---
+++ study/src/juyoungoh/programmers/lv0/_120893.java
@@ -0,0 +1,24 @@
+package juyoungoh.programmers.lv0;
+
+import java.util.Arrays;
+
+public class _120893 {
+
+    public static String solution(String my_string) {
+        String answer = "";
+
+        for (int i = 0; i < my_string.length(); i++) {
+            if (Character.isUpperCase(my_string.charAt(i))) {
+                answer += Character.toLowerCase(my_string.charAt(i));
+            } else if (Character.isLowerCase(my_string.charAt(i))) {
+                answer += Character.toUpperCase(my_string.charAt(i));
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("cccCCC"));
+        System.out.println(solution("abCdEfghIJ"));
+    }
+}
