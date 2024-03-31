130dcec7cd87d8d2060503e969e16e66c5dce3d1
ohjuyoungg <ejrm12@naver.com>
2024-03-31 15:25:48 +0900

    프로그래머스: rny_string (240331)

---
+++ study/src/juyoungoh/programmers/lv0/_181863.java
@@ -0,0 +1,20 @@
+package juyoungoh.programmers.lv0;
+
+public class _181863 {
+    public static String solution(String rny_string) {
+        String answer = "";
+        if (rny_string.contains("m")) {
+             answer = rny_string.replace("m", "rn");
+             return answer;
+        } else {
+            return rny_string;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("masterpiece"));
+        System.out.println(solution("programmers"));
+        System.out.println(solution("jerry"));
+        System.out.println(solution("burn"));
+    }
+}
