200e01355105cfbe3cc3418403f7e3052e417f62
ohjuyoungg <ejrm12@naver.com>
2024-04-02 13:06:48 +0900

    프로그래머스: 배열의 길이에 따라 다른 연산하기 (240402)

--- study/src/juyoungoh/programmers/lv0/_181854.java
+++ study/src/juyoungoh/programmers/lv0/_181854.java
@@ -1,2 +1,33 @@
-package juyoungoh.programmers.lv0;public class _181854 {
+package juyoungoh.programmers.lv0;
+
+public class _181854 {
+
+    public static int[] solution(int[] arr, int n) {
+        int[] answer = new int[arr.length];
+
+        if (arr.length % 2 == 0) {
+            for (int i = 0; i < arr.length; i++) {
+                if (i % 2 != 0) {
+                    answer[i] = arr[i] + n;
+                } else {
+                    answer[i] = arr[i];
+                }
+            }
+        } else {
+            for (int i = 0; i < arr.length; i++) {
+                if (i % 2 == 0) {
+                    answer[i] = arr[i] + n;
+                } else {
+                    answer[i] = arr[i];
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{2}, 2));
+        System.out.println(solution(new int[]{49, 12, 100, 276, 33}, 27)); // [76, 12, 127, 276, 60]
+        System.out.println(solution(new int[]{444, 555, 666, 777}, 100)); // [444, 655, 666, 877]
+    }
 }
