9692bdf8cef5679fca8e9fc16af206d595e92ff8
ohjuyoungg <ejrm12@naver.com>
2024-04-05 17:28:06 +0900

    프로그래머스: 배열의 원소 삭제하기 (240405) + 다른 사람 문제 풀이

--- study/src/juyoungoh/programmers/lv0/_181844.java
+++ study/src/juyoungoh/programmers/lv0/_181844.java
@@ -1,6 +1,9 @@
 package juyoungoh.programmers.lv0;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class _181844 {
 
@@ -37,10 +40,31 @@ public class _181844 {
         return answer2;
     }
 
+    /**
+     * 다른 사람의 풀이
+     */
+    public static int[] solution2(int[] arr, int[] delete_list) {
+        List<Integer> list = new ArrayList<>();
+        for (int n : arr) {
+            list.add(n);
+        }
+        for (int n : delete_list) {
+            list.remove((Integer) n);
+        }
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+
+    public static int[] solution3(int[] arr, int[] delete_list) {
+        return IntStream.of(arr).filter(i -> !IntStream.of(delete_list).anyMatch(s -> s == i)).toArray();
+    }
 
     public static void main(String[] args) {
         System.out.println(
-            Arrays.toString(solution(new int[]{293, 1000, 395, 678, 94}, new int[]{94, 777, 104, 1000, 1, 12})));
+            Arrays.toString(solution3(new int[]{293, 1000, 395, 678, 94}, new int[]{94, 777, 104, 1000, 1, 12})));
         System.out.println(Arrays.toString(solution(new int[]{110, 66, 439, 785, 1}, new int[]{377, 823, 119, 43})));
     }
 }
