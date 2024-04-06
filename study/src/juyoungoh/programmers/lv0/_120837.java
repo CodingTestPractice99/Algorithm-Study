4500f5874e59d4a06a19d5b2946db92f6ca15911
ohjuyoungg <ejrm12@naver.com>
2024-04-06 16:18:23 +0900

    프로그래머스: 개미 군단 (240406)

--- study/src/juyoungoh/programmers/lv0/_120837.java
+++ study/src/juyoungoh/programmers/lv0/_120837.java
@@ -1,2 +1,32 @@
-package juyoungoh.programmers.lv0;public class _120837 {
+package juyoungoh.programmers.lv0;
+
+/**
+ * 장군개미 - 5 병정개미 - 3 일개미 - 1
+ */
+public class _120837 {
+
+    public static int solution(int hp) {
+        int answer = 0;
+        int hp2 = hp;
+
+        while (hp2 >= 1) {
+            if (hp2 >= 5) {
+                answer += hp2 / 5;
+                hp2 = hp2 % 5;
+            } else if (hp2 >= 3) {
+                answer += hp2 / 3;
+                hp2 = hp2 % 3;
+            } else if (hp2 >= 1) {
+                answer += hp2 / 1;
+                hp2 = hp2 % 1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        // System.out.println(solution(23)); // 5
+        System.out.println(solution(24)); // 6
+        System.out.println(solution(999)); // 201
+    }
 }
