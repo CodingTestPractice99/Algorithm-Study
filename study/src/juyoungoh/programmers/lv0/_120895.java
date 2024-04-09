2af754407bf21022c93b5fc6208aafb7a053b997
ohjuyoungg <ejrm12@naver.com>
2024-04-09 15:01:10 +0900

    프로그래머스: 인덱스 바꾸기 (240409)

--- study/src/juyoungoh/programmers/lv0/_120895.java
+++ study/src/juyoungoh/programmers/lv0/_120895.java
@@ -1,2 +1,23 @@
-package juyoungoh.programmers.lv0;public class _120895 {
+package juyoungoh.programmers.lv0;
+
+public class _120895 {
+
+    public static String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        for (int i = 0; i < my_string.length(); i++) {
+            if (i == num1) {
+                answer += my_string.charAt(num2);
+            } else if (i == num2) {
+                answer += my_string.charAt(num1);
+            } else {
+                answer += my_string.charAt(i);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("hello", 1, 2));
+        System.out.println(solution("I love you", 3, 6));
+    }
 }
