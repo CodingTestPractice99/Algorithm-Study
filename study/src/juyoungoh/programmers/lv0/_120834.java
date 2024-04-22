fb1089fd742105ef79ee9ac22d7419d9ccc9200f
ohjuyoungg <ejrm12@naver.com>
2024-04-22 21:55:24 +0900

    프로그래머스: 외계행성의 나이 (240422)

--- study/src/juyoungoh/programmers/lv0/_120834.java
+++ study/src/juyoungoh/programmers/lv0/_120834.java
@@ -1,2 +1,19 @@
-package juyoungoh.programmers.lv0;public class _120834 {
+package juyoungoh.programmers.lv0;
+
+public class _120834 {
+    public static String solution(int age) {
+        String answer = "";
+        String ages = String.valueOf(age);
+        String arr[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+        for (int i = 0; i < ages.length(); i++) {
+            answer += arr[Integer.parseInt(String.valueOf(ages.charAt(i)))];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(23)); // cd
+        System.out.println(solution(51)); // fb
+        System.out.println(solution(1000)); // baa
+    }
 }
