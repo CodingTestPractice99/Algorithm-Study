2c017fa09ebba4483cdbd02c5f5a653c3f5f6aee
ohjuyoungg <ejrm12@naver.com>
2024-05-04 15:26:42 +0900

    프로그래머스: l로 만들기 (240504)

--- study/src/juyoungoh/programmers/lv0/_181834.java
+++ study/src/juyoungoh/programmers/lv0/_181834.java
@@ -4,17 +4,20 @@ public class _181834 {
 
     public static String solution(String myString) {
         String answer = "";
-        String abc = "abcdefghijk";
+        String abcd = "abcdefghijk";
+        String abc[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"};
         for (int i = 0; i < myString.length(); i++) {
-           /* if (abc.contains(myString.charAt(i))) {
-
-            }*/
+            if (abcd.contains(String.valueOf(myString.charAt(i)))) {
+                answer += "l";
+            } else {
+                answer += myString.charAt(i);
+            }
         }
         return answer;
     }
 
     public static void main(String[] args) {
-        System.out.println(solution("abcdevwxyz"));
-        System.out.println(solution("jjnnllkkmm"));
+        System.out.println(solution("abcdevwxyz")); // lllllvwxyz
+        System.out.println(solution("jjnnllkkmm")); // llnnllllmm
     }
 }
