fd78e604ed226a2d8c2db5e5c3ed087ddc98735d
ohjuyoungg <ejrm12@naver.com>
2024-05-06 18:52:49 +0900

    프로그래머스: 특별한 이차원 배열1 (240506)

--- study/src/juyoungoh/programmers/lv0/_181833.java
+++ study/src/juyoungoh/programmers/lv0/_181833.java
@@ -1,2 +1,31 @@
-package juyoungoh.programmers.lv0;public class _181833 {
+package juyoungoh.programmers.lv0;
+
+import java.util.Arrays;
+
+public class _181833 {
+    public static int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+        int zero = 0;
+        int one = 1;
+
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer.length; j++) {
+                if (i == j) {
+                    answer[i][j] = one;
+                } else {
+                    answer[i][j] = zero;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(3)));
+        // [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+        System.out.println(Arrays.toString(solution(6)));
+        // [[1, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 1]]
+        System.out.println(Arrays.toString(solution(1)));
+        // [[1]]
+    }
 }
