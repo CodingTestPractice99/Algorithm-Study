2c03b7bdfdd4cfd4e43ee61cbca67696a10919ca
ohjuyoungg <ejrm12@naver.com>
2024-05-29 18:24:02 +0900

    프로그래머스: 간단한 식 계산하기 (240529)

--- study/src/juyoungoh/programmers/lv0/_181865.java
+++ study/src/juyoungoh/programmers/lv0/_181865.java
@@ -1,2 +1,27 @@
-package juyoungoh.programmers.lv0;public class _181865 {
+package juyoungoh.programmers.lv0;
+
+public class _181865 {
+
+    public static int solution(String binomial) {
+        int answer = 0;
+        String array[] = binomial.split(" ");
+
+        int a = Integer.parseInt(array[0]);
+        int b = Integer.parseInt(array[2]);
+
+        if (array[1].equals("+")) {
+            return a + b;
+        } else if (array[1].equals("-")) {
+            return a - b;
+        } else if (array[1].equals("*")) {
+            return a * b;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("43 + 12"));
+        System.out.println(solution("0 - 7777"));
+        System.out.println(solution("40000 * 40000"));
+    }
 }
