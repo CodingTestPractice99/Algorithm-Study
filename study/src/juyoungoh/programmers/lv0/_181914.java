53f0b1470f500bb0aeab61c7b9a79da50e3540f0
ohjuyoungg <ejrm12@naver.com>
2024-06-05 23:02:57 +0900

    프로그래머스: 9로 나눈 나머지 (240605)

--- study/src/juyoungoh/programmers/lv0/_181914.java
+++ study/src/juyoungoh/programmers/lv0/_181914.java
@@ -1,2 +1,18 @@
-package juyoungoh.programmers.lv0;public class _181914 {
+package juyoungoh.programmers.lv0;
+
+public class _181914 {
+
+    public static int solution(String number) {
+        int num = 0;
+
+        for (int i = 0; i < number.length(); i++) {
+            num += Integer.parseInt(String.valueOf(number.charAt(i)));
+        }
+        return num % 9;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("123")); // 6
+        System.out.println(solution("78720646226947352489")); // 2
+    }
 }
