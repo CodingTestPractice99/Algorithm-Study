4fdd3c894165a5e5a6d0bc994f2d9a2eea35eea5
ohjuyoungg <ejrm12@naver.com>
2024-06-06 15:01:58 +0900

    프로그래머스: 문자열 잘라서 정렬하기 (240606)

--- study/src/juyoungoh/programmers/lv0/_181866.java
+++ study/src/juyoungoh/programmers/lv0/_181866.java
@@ -1,2 +1,35 @@
-package juyoungoh.programmers.lv0;public class _181866 {
+package juyoungoh.programmers.lv0;
+
+import java.util.Arrays;
+
+public class _181866 {
+
+    public static String[] solution(String myString) {
+        String answer[] = myString.split("x");
+        int count = 0;
+        int index = 0;
+
+        for (int i = 0; i < answer.length; i++) {
+            if (answer[i].isBlank()) {
+                count++;
+            }
+        }
+
+        String answer2[] = new String[answer.length - count];
+
+        for (int i = 0; i < answer.length; i++) {
+            if (!(answer[i].isBlank())) {
+                answer2[index] = answer[i];
+                index++;
+            }
+        }
+        Arrays.sort(answer2);
+        return answer2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution("dxccxbbbxxaaaa")));
+        System.out.println(Arrays.toString(solution("axbxcxdx")));
+        System.out.println(Arrays.toString(solution("dxccxbbbxaaaa")));
+    }
 }
