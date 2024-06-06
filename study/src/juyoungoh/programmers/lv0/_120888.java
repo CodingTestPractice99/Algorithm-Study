8993ed2b27e6d5527d5223b576915126fb76c752
ohjuyoungg <ejrm12@naver.com>
2024-06-06 15:10:43 +0900

    프로그래머스: 중복된 문자 제거 (240606)

--- study/src/juyoungoh/programmers/lv0/_120888.java
+++ study/src/juyoungoh/programmers/lv0/_120888.java
@@ -1,2 +1,22 @@
-package juyoungoh.programmers.lv0;public class _120888 {
+package juyoungoh.programmers.lv0;
+
+public class _120888 {
+
+    public static String solution(String my_string) {
+        String str = my_string;
+        char c[] = str.toCharArray();
+        String answer = "";
+
+        for (int i = 0; i < c.length; i++) {
+            if (!(answer.contains(String.valueOf(c[i])))) {
+                answer += c[i];
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("people"));
+        System.out.println(solution("We are the world"));
+    }
 }
