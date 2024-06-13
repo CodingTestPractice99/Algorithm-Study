639bcb36fe2b13032c024d4f2069ac8cd2034ea1
ohjuyoungg <ejrm12@naver.com>
2024-06-13 14:17:04 +0900

    프로그래머스: 1로 만들기 (240613)

--- study/src/juyoungoh/programmers/lv0/_181880.java
+++ study/src/juyoungoh/programmers/lv0/_181880.java
@@ -1,2 +1,27 @@
-package juyoungoh.programmers.lv0;public class _181880 {
+package juyoungoh.programmers.lv0;
+
+public class _181880 {
+
+    public static int solution(int[] num_list) {
+        int answer = 0;
+        int num = 0;
+        for (int i = 0; i < num_list.length; i++) {
+            num = num_list[i];
+            while (num > 1) {
+                if (num % 2 == 0) {
+                    num = num / 2;
+                    answer++;
+                } else if (num % 2 != 0) {
+                    num = (num - 1) / 2;
+                    answer++;
+                }
+            }
+            num = 0;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{12, 4, 15, 1, 14}));
+    }
 }
