2f6d2c2f8ce805bb982c9309dd15d457a0527eda
ohjuyoungg <ejrm12@naver.com>
2024-06-13 21:40:39 +0900

    프로그래머스: 문자열 반복해서 출력하기 (240613)

---
+++ study/src/juyoungoh/programmers/lv0/_181950.java
@@ -0,0 +1,18 @@
+package juyoungoh.programmers.lv0;
+
+import java.util.Scanner;
+
+public class _181950 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+
+        String answer = "";
+        for (int i = 0; i < n; i++) {
+                answer += str;
+        }
+        System.out.println(answer);
+    }
+}
