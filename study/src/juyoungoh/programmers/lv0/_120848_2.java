e073e277b087ba238c76715c250cf10cdf8687b3
ohjuyoungg <ejrm12@naver.com>
2024-06-13 13:50:11 +0900

    프로그래머스: 팩토리얼 (240613)

--- study/src/juyoungoh/programmers/lv0/_120848_2.java
+++ study/src/juyoungoh/programmers/lv0/_120848_2.java
@@ -1,2 +1,26 @@
-package juyoungoh.programmers.lv0;public class _120848_2 {
+package juyoungoh.programmers.lv0;
+
+public class _120848_2 {
+
+    public static int solution(int n) {
+        int factorial = 1;
+        int num = 1;
+
+        while (factorial < n) {
+            factorial = factorial * num;
+            num++;
+            if (factorial == n) {
+                return num - 1;
+            } else if (factorial > n) {
+                return num - 2;
+            }
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(24));
+        System.out.println(solution(180));
+        System.out.println(solution(7));
+    }
 }
