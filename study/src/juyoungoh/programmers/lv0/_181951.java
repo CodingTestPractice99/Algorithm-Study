6b789195735bc3374993d3aec67b471c74943a4b
ohjuyoungg <ejrm12@naver.com>
2024-06-14 17:02:56 +0900

    프로그래머스: a와 b 출력하기 (240614)

---
+++ study/src/juyoungoh/programmers/lv0/_181951.java
@@ -0,0 +1,15 @@
+package juyoungoh.programmers.lv0;
+
+import java.util.Scanner;
+
+public class _181951 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
