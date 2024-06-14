2b19783be26c5c7f34770089164f66b63a81aa67
ohjuyoungg <ejrm12@naver.com>
2024-06-14 16:52:35 +0900

    프로그래머스: 한 번만 등장한 문자 (240614)

--- study/src/juyoungoh/programmers/lv0/_120896.java
+++ study/src/juyoungoh/programmers/lv0/_120896.java
@@ -1,2 +1,38 @@
-package juyoungoh.programmers.lv0;public class _120896 {
+package juyoungoh.programmers.lv0;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class _120896 {
+
+    public static String solution(String s) {
+        String answer = "";
+        int count = 0;
+        ArrayList<String> str = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            str.add(String.valueOf(s.charAt(i)));
+        }
+
+        for (int i = 0; i < str.size(); i++) {
+            for (int j = 0; j < str.size(); j++) {
+                if (str.get(i).equals(str.get(j))) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                answer += str.get(i);
+            }
+            count = 0;
+        }
+
+        char[] charArray = answer.toCharArray();
+        Arrays.sort(charArray);
+        return new String(charArray);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("abcabcadc")); // d
+        System.out.println(solution("abdc")); // abcd
+        System.out.println(solution("hello")); // eho
+    }
 }
