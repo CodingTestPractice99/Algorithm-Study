1eaf551423ea189fe1d9da597338ad53e21eab26
ohjuyoungg <ejrm12@naver.com>
2024-06-14 12:10:01 +0900

    프로그래머스: 진료순서 정하기 (240614)

--- study/src/juyoungoh/programmers/lv0/_120835_1.java
+++ study/src/juyoungoh/programmers/lv0/_120835_1.java
@@ -1,2 +1,24 @@
-package juyoungoh.programmers.lv0;public class _120835_1 {
+package juyoungoh.programmers.lv0;
+
+import java.util.Arrays;
+
+public class _120835_1 {
+
+    public static int[] solution(int[] emergency) {
+        int[] answer = new int[emergency.length];
+        for (int i = 0; i < emergency.length; i++) {
+            for (int j = 0; j < emergency.length; j++) {
+                if (emergency[i] <= emergency[j]) {
+                    answer[i] += 1;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[]{3, 76, 24}))); // 3, 1, 2
+        System.out.println(Arrays.toString(solution(new int[]{1, 2, 3, 4, 5, 6, 7})));
+        System.out.println(Arrays.toString(solution(new int[]{30, 10, 23, 6, 100})));
+    }
 }
