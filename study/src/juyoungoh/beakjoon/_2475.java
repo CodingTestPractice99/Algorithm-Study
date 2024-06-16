ae0a9093490f3c796059de018e9c98e05b95c9b9
ohjuyoungg <ejrm12@naver.com>
2024-06-16 14:35:50 +0900

    백준: 검증수 (240616)

--- study/src/juyoungoh/beakjoon/_2475.java
+++ study/src/juyoungoh/beakjoon/_2475.java
@@ -1,2 +1,26 @@
-package juyoungoh.beakjoon;public class _2475 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2475 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str[] = (br.readLine().split(" "));
+        int num[] = new int[5];
+        int sum = 0;
+        int answer = 0;
+
+        for (int i = 0; i < str.length; i++) {
+            num[i] = Integer.parseInt(str[i]) * Integer.parseInt(str[i]);
+        }
+
+        for (int i = 0; i < num.length; i++) {
+            sum += num[i];
+        }
+        answer = sum % 10;
+        System.out.println(answer);
+    }
 }
