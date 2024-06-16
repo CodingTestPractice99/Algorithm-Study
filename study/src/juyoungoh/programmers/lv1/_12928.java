3a0fb042f326a4b95afcd40224b06fa726b75ba7
ohjuyoungg <ejrm12@naver.com>
2024-06-16 10:07:35 +0900

    프로그래머스: 약수의 합 (240616)

---
+++ study/src/juyoungoh/programmers/lv1/_12928.java
@@ -0,0 +1,19 @@
+package juyoungoh.programmers.lv1;
+
+public class _12928 {
+    public static int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(12));
+        System.out.println(solution(5));
+    }
+}
