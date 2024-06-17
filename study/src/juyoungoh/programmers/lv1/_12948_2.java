9fc7931ddc9bc9fe74a93cf53af660577069f057
ohjuyoungg <ejrm12@naver.com>
2024-06-17 12:15:20 +0900

    프로그래머스: 핸드폰 번호 가리기 (240617)

--- study/src/juyoungoh/programmers/lv1/_12948_2.java
+++ study/src/juyoungoh/programmers/lv1/_12948_2.java
@@ -1,2 +1,13 @@
-package juyoungoh.programmers.lv1;public class _12948_2 {
+package juyoungoh.programmers.lv1;
+
+public class _12948_2 {
+
+    public static String solution(String phone_number) {
+        return phone_number.replaceAll(".(?=.{4})", "*");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("01033334444")); // *******4444
+        System.out.println(solution("027778888")); // *****8888
+    }
 }
