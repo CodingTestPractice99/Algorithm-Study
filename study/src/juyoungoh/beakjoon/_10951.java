7bbed16efeeeb9ec357c0edd644f36bf58c47d74
ohjuyoungg <ejrm12@naver.com>
2024-06-17 14:13:31 +0900

    백준: A+B - 4 (240617)

--- study/src/juyoungoh/beakjoon/_10951.java
+++ study/src/juyoungoh/beakjoon/_10951.java
@@ -1,39 +1,17 @@
 package juyoungoh.beakjoon;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class _10951 {
 
-    public static void exScanner() {
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextInt()) {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-
-            System.out.println(a+b);
-        }
-        scanner.close();
-    }
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
 
-        String str;
-
-        while ((str = br.readLine()) != null) {
-            st = new StringTokenizer(str, " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            sb.append(a+b).append("\n");
-
+            System.out.println(a + b);
         }
-        System.out.println(sb);
     }
 }
