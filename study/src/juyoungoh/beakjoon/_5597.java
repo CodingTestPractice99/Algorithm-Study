e9fc16de756bf8d1612d8e6586a1f70cebd80f14
ohjuyoungg <ejrm12@naver.com>
2024-06-17 14:02:46 +0900

    백준: 과제 안 내신 분..? (240617)

--- study/src/juyoungoh/beakjoon/_5597.java
+++ study/src/juyoungoh/beakjoon/_5597.java
@@ -1,2 +1,22 @@
-package juyoungoh.beakjoon;public class _5597 {
+package juyoungoh.beakjoon;
+
+import java.util.Scanner;
+
+public class _5597 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int answerNum[] = new int[30];
+
+        for (int i = 0; i < 28; i++) {
+            int num = sc.nextInt();
+            answerNum[num - 1] = num;
+        }
+
+        for (int i = 0; i < answerNum.length; i++) {
+            if (answerNum[i] == 0) {
+                System.out.println(i + 1);
+            }
+        }
+    }
 }
