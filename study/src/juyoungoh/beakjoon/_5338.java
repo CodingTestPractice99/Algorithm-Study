e85c20194bb70bc30629683fc42be1cc7749aab8
ohjuyoungg <ejrm12@naver.com>
2024-06-17 21:18:28 +0900

    백준: 마이크로소프트 로고 (240617)

---
+++ study/src/juyoungoh/beakjoon/_5338.java
@@ -0,0 +1,12 @@
+package juyoungoh.beakjoon;
+
+public class _5338 {
+
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._\n"
+            + "'-..-'|   ||   |\n"
+            + "'-..-'|_.-;;-._|\n"
+            + "'-..-'|   ||   |\n"
+            + "'-..-'|_.-''-._|");
+    }
+}
