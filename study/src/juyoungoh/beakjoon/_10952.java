9e225c5181dee736b40ccd8f8c69de548a3f19f9
ohjuyoungg <ejrm12@naver.com>
2024-06-17 11:10:02 +0900

    백준: A+B-5 (240617)

--- study/src/juyoungoh/beakjoon/_10952.java
+++ study/src/juyoungoh/beakjoon/_10952.java
@@ -1,27 +1,23 @@
 package juyoungoh.beakjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class _10952 {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
 
         while (true) {
-            st = new StringTokenizer(br.readLine(), " "); // BufferedReader를 사용하여 한 줄을 읽고, StringTokenizer를 사용하여 공백을 기준으로 문자열을 분리한다.
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            if (a==0 && b==0) {
+            String str[] = sc.nextLine().split(" ");
+            if ((str[0].equals("0") && str[1].equals("0"))) {
                 break;
             }
-            sb.append((a+b)).append('\n');
+            for (int i = 0; i < str.length; i++) {
+                sum = Integer.parseInt(str[0]) + Integer.parseInt(str[1]);
+            }
+            System.out.println(sum);
+            sum = 0;
         }
-        System.out.println(sb);
     }
 }
