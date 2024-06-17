0c8d84b321cbd4004668b17d502b25d6e4bea6d4
ohjuyoungg <ejrm12@naver.com>
2024-06-17 20:55:03 +0900

    백준: 수 정렬하기 (240617)

---
+++ study/src/juyoungoh/beakjoon/_2750.java
@@ -0,0 +1,26 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class _2750 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int array[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(array);
+        for (int i = 0; i < array.length; i++) {
+            sb.append(array[i] + "\n");
+        }
+        System.out.print(sb);
+    }
+}
