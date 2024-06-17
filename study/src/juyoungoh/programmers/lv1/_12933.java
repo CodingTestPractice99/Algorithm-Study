94e87d1e9afcf3d0f31ccd746e4403ee355f9128
ohjuyoungg <ejrm12@naver.com>
2024-06-17 18:09:20 +0900

    프로그래머스: 정수 내림차순으로 배치하기 (240617)

--- study/src/juyoungoh/programmers/lv1/_12933.java
+++ study/src/juyoungoh/programmers/lv1/_12933.java
@@ -1,35 +1,29 @@
 package juyoungoh.programmers.lv1;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 
 public class _12933 {
 
     public static long solution(long n) {
         long answer = 0;
-        String answer2 = "";
+        String answerStr = "";
         String str = String.valueOf(n);
-        long arr2[] = new long[str.length()];
+
         ArrayList<Long> arr = new ArrayList<>();
 
-        for (int i = 0; i < arr2.length; i++) {
-            arr2[i] = Long.parseLong(String.valueOf(str.charAt(i)));
+        for (int i = 0; i < str.length(); i++) {
+            arr.add(Long.parseLong(String.valueOf(str.charAt(i))));
         }
 
-        for (int i = 0; i < arr2.length; i++) {
-            arr.add(arr2[i]);
-        }
-
-        Arrays.sort(arr2);
         Collections.sort(arr);
         Collections.reverse(arr);
 
         for (int i = 0; i < arr.size(); i++) {
-            answer2 += String.valueOf(arr.get(i));
+            answerStr += String.valueOf(arr.get(i));
         }
 
-        answer = Long.parseLong(answer2);
+        answer = Long.parseLong(answerStr);
         return answer;
     }
 
