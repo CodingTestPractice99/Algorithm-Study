4338c6a30374df457cce90cbea1810d97819d369
ohjuyoungg <ejrm12@naver.com>
2024-06-18 20:32:11 +0900

    백준: 새싹 (240618)

---
+++ study/src/juyoungoh/beakjoon/_25083.java
@@ -0,0 +1,13 @@
+package juyoungoh.beakjoon;
+
+public class _25083 {
+
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7\n"
+            + "r`-_   ,'  ,/\n"
+            + " \\. \". L_r'\n"
+            + "   `~\\/\n"
+            + "      |\n"
+            + "      |");
+    }
+}
