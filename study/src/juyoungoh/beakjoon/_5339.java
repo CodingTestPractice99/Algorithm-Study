0425adee54d67cbe7994bd8fcf137a38a43f89bd
ohjuyoungg <ejrm12@naver.com>
2024-06-18 21:05:44 +0900

    백준: 콜센터 (240618)

---
+++ study/src/juyoungoh/beakjoon/_5339.java
@@ -0,0 +1,17 @@
+package juyoungoh.beakjoon;
+
+public class _5339 {
+
+    public static void main(String[] args) {
+        System.out.println("     /~\\\n"
+            + "    ( oo|\n"
+            + "    _\\=/_\n"
+            + "   /  _  \\\n"
+            + "  //|/.\\|\\\\\n"
+            + " ||  \\ /  ||\n"
+            + "============\n"
+            + "|          |\n"
+            + "|          |\n"
+            + "|          |");
+    }
+}
