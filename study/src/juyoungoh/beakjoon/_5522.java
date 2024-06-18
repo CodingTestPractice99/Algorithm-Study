3d37918da8c31a75f51a610e0972f6d7c3f2c73d
ohjuyoungg <ejrm12@naver.com>
2024-06-18 21:02:09 +0900

    백준: 카드 게임 (240618)

---
+++ study/src/juyoungoh/beakjoon/_5522.java
@@ -0,0 +1,20 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _5522 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        int e = Integer.parseInt(br.readLine());
+
+        System.out.println(a + b + c + d + e);
+    }
+}
