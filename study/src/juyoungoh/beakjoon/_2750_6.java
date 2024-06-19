7ea2d013b7aa333961c607a22ea8aae1c31d8691
ohjuyoungg <ejrm12@naver.com>
2024-06-19 17:01:14 +0900

    백준: 수 정렬하기 (240619)

---
+++ study/src/juyoungoh/beakjoon/_2750_6.java
@@ -0,0 +1,29 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class _2750_6 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        // 정렬 메소드
+        Arrays.sort(arr);
+
+        for (int i : arr) {
+            sb.append(i).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
