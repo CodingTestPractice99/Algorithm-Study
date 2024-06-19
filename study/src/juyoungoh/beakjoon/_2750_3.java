9c32738c2e4f98bfb1cb39382133619485fd325a
ohjuyoungg <ejrm12@naver.com>
2024-06-19 16:50:46 +0900

    백준: 수 정렬하기 (240619)

---
+++ study/src/juyoungoh/beakjoon/_2750_3.java
@@ -0,0 +1,36 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2750_3 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        // Selection sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        for (int i : arr) {
+            sb.append(i).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
