d53b69e7aa3754c89bdb443dc0744d418a3eba15
ohjuyoungg <ejrm12@naver.com>
2024-06-25 14:10:38 +0900

    백준: 그대로 출력하기 (240625)

---
+++ study/src/juyoungoh/beakjoon/_11718.java
@@ -0,0 +1,18 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _11718 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
+        br.close();
+    }
+}
