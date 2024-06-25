4a2c1b1afacd72312761790afaf0c2c9b6ce3b70
ohjuyoungg <ejrm12@naver.com>
2024-06-25 14:48:28 +0900

    commit

--- study/src/juyoungoh/beakjoon/_10809.java
+++ study/src/juyoungoh/beakjoon/_10809.java
@@ -1,2 +1,23 @@
-package juyoungoh.beakjoon;public class _10809 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _10809 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        int alphabet[] = new int[26];
+
+        for (int i = 0; i < alphabet.length; i++) {
+            alphabet[i] = -1;
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+
+        }
+    }
 }
