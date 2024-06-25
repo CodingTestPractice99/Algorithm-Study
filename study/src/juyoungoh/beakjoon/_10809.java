f3bebc753ea0fa28ee0bb2e994350240659b49d3
ohjuyoungg <ejrm12@naver.com>
2024-06-25 15:06:12 +0900

    백준: 알파벳 찾기 (240625)

--- study/src/juyoungoh/beakjoon/_10809.java
+++ study/src/juyoungoh/beakjoon/_10809.java
@@ -10,14 +10,52 @@ public class _10809 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
 
-        int alphabet[] = new int[26];
+        int alphabetNum[] = new int[26];
+        String alphabet[] = new String[26];
 
-        for (int i = 0; i < alphabet.length; i++) {
-            alphabet[i] = -1;
+        alphabet[0] = "a";
+        alphabet[1] = "b";
+        alphabet[2] = "c";
+        alphabet[3] = "d";
+        alphabet[4] = "e";
+        alphabet[5] = "f";
+        alphabet[6] = "g";
+        alphabet[7] = "h";
+        alphabet[8] = "i";
+        alphabet[9] = "j";
+        alphabet[10] = "k";
+        alphabet[11] = "l";
+        alphabet[12] = "m";
+        alphabet[13] = "n";
+        alphabet[14] = "o";
+        alphabet[15] = "p";
+        alphabet[16] = "q";
+        alphabet[17] = "r";
+        alphabet[18] = "s";
+        alphabet[19] = "t";
+        alphabet[20] = "u";
+        alphabet[21] = "v";
+        alphabet[22] = "w";
+        alphabet[23] = "x";
+        alphabet[24] = "y";
+        alphabet[25] = "z";
+
+        for (int i = 0; i < alphabetNum.length; i++) {
+            alphabetNum[i] = -1;
         }
 
         for (int i = 0; i < input.length(); i++) {
-
+            for (int j = 0; j < alphabet.length; j++) {
+                if (String.valueOf(input.charAt(i)).equals(alphabet[j]) && alphabetNum[j] == -1) {
+                    alphabetNum[j] = i;
+                }
+            }
         }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < alphabetNum.length; i++) {
+            sb.append(alphabetNum[i]).append(" ");
+        }
+        System.out.println(sb);
     }
 }
