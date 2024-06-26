b14d608f3a08022c94b01dd6fb19b3d7d07b4ea7
ohjuyoungg <ejrm12@naver.com>
2024-06-26 12:00:10 +0900

    백준: 와이버스 부릉부릉 (240626)

---
+++ study/src/juyoungoh/beakjoon/_14645.java
@@ -0,0 +1,19 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _14645 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            break;
+        }
+        System.out.println("비와이");
+        br.close();
+    }
+}
