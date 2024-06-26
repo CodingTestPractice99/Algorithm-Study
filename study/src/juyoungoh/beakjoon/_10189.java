93aaa0b8522ca3b2b4611133a89d8f0071181784
ohjuyoungg <ejrm12@naver.com>
2024-06-26 11:52:23 +0900

    백준: Hook (240626)

---
+++ study/src/juyoungoh/beakjoon/_10189.java
@@ -0,0 +1,12 @@
+package juyoungoh.beakjoon;
+
+
+public class _10189 {
+
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #\n"
+            + "#### #  # #  # # #\n"
+            + "#### #  # #  # # #\n"
+            + "#  # #### #### #  #");
+    }
+}
