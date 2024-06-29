acb0224eb5a111c0f88f768cb2ff621dc2d55c2f
ohjuyoungg <ejrm12@naver.com>
2024-06-29 14:45:47 +0900

    백준: 별 찍기 - 9 (240629)

--- study/src/juyoungoh/beakjoon/_2446.java
+++ study/src/juyoungoh/beakjoon/_2446.java
@@ -1,2 +1,33 @@
-package juyoungoh.beakjoon;public class _2446 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2446 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) { // 1, 2, 3, 4, 5
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = n * 2; j > i * 2 - 1; j--) { // 9, 7, 5, 3, 1
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 2; i <= n; i++) { // 행 2, 3, 4, 5
+            for (int j = n - 1; j > i - 1; j--) { // 3, 2, 1
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i * 2 - 1; j++) { // 3, 5, 7, 9
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
