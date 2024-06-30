4aa5d8ae5585ca8cc29a0ab055d5b8236c67d72a
ohjuyoungg <ejrm12@naver.com>
2024-06-30 14:20:44 +0900

    백준: 별 찍기 - 15 (240630)

--- study/src/juyoungoh/beakjoon/_10990.java
+++ study/src/juyoungoh/beakjoon/_10990.java
@@ -8,24 +8,26 @@ public class _10990 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
         for (int i = 1; i <= n; i++) {
             for (int j = n; j > i; j--) { // 공백
-                System.out.print(" ");
+                sb.append(' ');
             }
             for (int k = 0; k < 1; k++) {
-                System.out.print("*");
+                sb.append('*');
             }
             for (int j = 1; j < i * 2 - 2; j++) { // 1, 3, 5
-                System.out.print(" ");
+                sb.append(' ');
             }
             if (i != 1) {
                 for (int j = 0; j < 1; j++) {
-                    System.out.print("*");
+                    sb.append('*');
                 }
             }
-            System.out.println();
+            sb.append('\n');
         }
+        System.out.print(sb);
     }
 }
