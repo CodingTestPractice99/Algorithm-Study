fd1e7d4f20616332f2912a5dffbd3f775e99182a
ohjuyoungg <ejrm12@naver.com>
2024-07-07 21:30:55 +0900

    백준: 특별한 학교 이름 (240707)

---
+++ study/src/juyoungoh/beakjoon/_27889.java
@@ -0,0 +1,24 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _27889 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        if (str.equals("NLCS")) {
+            System.out.println("North London Collegiate School");
+        } else if (str.equals("BHA")) {
+            System.out.println("Branksome Hall Asia");
+        } else if (str.equals("KIS")) {
+            System.out.println("Korea International School");
+        } else if (str.equals("SJA")) {
+            System.out.println("St. Johnsbury Academy");
+        }
+    }
+}
