6536453f23553fbedb4e4a529aa4f848d16224f7
ohjuyoungg <ejrm12@naver.com>
2024-07-17 15:24:28 +0900

    프로그래머스: 가운데 글자 가져오기 (240717)

--- study/src/juyoungoh/programmers/lv1/_12903.java
+++ study/src/juyoungoh/programmers/lv1/_12903.java
@@ -1,13 +1,14 @@
 package juyoungoh.programmers.lv1;
 
 public class _12903 {
+
     public static String solution(String s) {
         if (s.length() == 1 || s.length() == 2) {
             return s;
         } else if (s.length() % 2 == 0) {
             int a = s.length() / 2 - 1;
             String str1 = String.valueOf(s.charAt(a));
-            String str2 = String.valueOf(s.charAt(a+1));
+            String str2 = String.valueOf(s.charAt(a + 1));
             return str1 + str2;
         } else {
             int a = s.length() / 2;
