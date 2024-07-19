42893a8ed9c5e833fa400b04e8214cbfcf1d8fc4
ohjuyoungg <ejrm12@naver.com>
2024-07-19 21:17:37 +0900

    백준: 이상한 기호 (240719)

--- study/src/juyoungoh/beakjoon/_15964.java
+++ study/src/juyoungoh/beakjoon/_15964.java
@@ -1,2 +1,19 @@
-package juyoungoh.beakjoon;public class _15964 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _15964 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String strArr[] = str.split(" ");
+        long A = Long.parseLong(strArr[0]);
+        long B = Long.parseLong(strArr[1]);
+
+        long answer = (A+B) * (A-B);
+        System.out.println(answer);
+    }
 }
