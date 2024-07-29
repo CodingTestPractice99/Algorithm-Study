c23820c36bb77aefffa4566399a90e08d200e76c
ohjuyoungg <ejrm12@naver.com>
2024-07-29 15:13:11 +0900

    백준: 오늘 날짜 (240729)

---
+++ study/src/juyoungoh/beakjoon/_10699.java
@@ -0,0 +1,11 @@
+package juyoungoh.beakjoon;
+
+import java.time.LocalDate;
+
+public class _10699 {
+
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+    }
+}
