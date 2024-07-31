e740c87de55ef3348f4e018446c483144d72976a
ohjuyoungg <ejrm12@naver.com>
2024-07-31 15:46:33 +0900

    백준: 홍익대학교 (240731)

---
+++ study/src/juyoungoh/beakjoon/_16394.java
@@ -0,0 +1,13 @@
+package juyoungoh.beakjoon;
+
+import java.util.Scanner;
+
+public class _16394 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+
+        System.out.println(year - 1946);
+    }
+}
