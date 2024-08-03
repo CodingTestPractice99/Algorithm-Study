696684e46b2c1193ae97950485e9bae64e1b2fa2
ohjuyoungg <ejrm12@naver.com>
2024-08-03 20:59:11 +0900

    백준: 밀비 급일 (240803)

---
+++ study/src/juyoungoh/beakjoon/_11365.java
@@ -0,0 +1,22 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _11365 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String str = br.readLine();
+            if (str.equals("END")) {
+                break;
+            } else {
+                String reversed =new StringBuilder(str).reverse().toString();
+                System.out.println(reversed);
+            }
+        }
+    }
+}
