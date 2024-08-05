77b12f78f47c5f189369afa43a049b74e9ebd5e2
ohjuyoungg <ejrm12@naver.com>
2024-08-05 15:55:52 +0900

    백준: Hashing (240805)

--- study/src/juyoungoh/beakjoon/_15829.java
+++ study/src/juyoungoh/beakjoon/_15829.java
@@ -1,2 +1,32 @@
-package juyoungoh.beakjoon;public class _15829 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _15829 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long num = Long.parseLong(br.readLine());
+        String str = br.readLine();
+
+        char alphabet[] = str.toCharArray();
+
+        long mod = 1234567891;
+        long r = 31;
+        long sum = 0;
+
+        for (int i = 0; i < num; i++) {
+            long V = alphabet[i] - 'a' + 1;
+            long a2 = 1;
+            for (int j = 0; j < i; j++) {
+                a2 = (r * a2) % mod;
+            }
+            long temp = V * a2 % mod;
+            sum += temp % mod;
+        }
+        System.out.println(sum % mod);
+    }
 }
