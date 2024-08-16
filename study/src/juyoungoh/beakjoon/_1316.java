29d382eced24ec10a0a3fe982df1b4854be73a77
ohjuyoungg <ejrm12@naver.com>
2024-08-16 14:20:18 +0900

    백준: 그룹 단어 체커 (240816)

--- study/src/juyoungoh/beakjoon/_1316.java
+++ study/src/juyoungoh/beakjoon/_1316.java
@@ -1,2 +1,44 @@
-package juyoungoh.beakjoon;public class _1316 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _1316 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int count = 0;
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            if (check()) {
+                count++;
+            }
+        }
+        System.out.print(count);
+    }
+
+    public static boolean check() throws IOException {
+        boolean[] check = new boolean[26];
+        int prev = 0;
+        String str = br.readLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            int now = str.charAt(i);
+
+            if (prev != now) {
+                if (!check[now - 'a']) {
+                    check[now - 'a'] = true;
+                    prev = now;
+                } else {
+                    return false;
+                }
+            } else {
+                continue;
+            }
+        }
+        return true;
+    }
 }
