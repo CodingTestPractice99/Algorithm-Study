90d55bb7bfa91c0960146c2919ebb78f18b9ff88
ohjuyoungg <ejrm12@naver.com>
2024-08-18 15:21:26 +0900

    백준: 팩토리얼 (240818)

--- study/src/juyoungoh/beakjoon/_10872.java
+++ study/src/juyoungoh/beakjoon/_10872.java
@@ -1,2 +1,22 @@
-package juyoungoh.beakjoon;public class _10872 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _10872 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = factorial(n);
+        System.out.println(sum);
+    }
+
+    public static int factorial(int n) {
+        if (n <= 1) return 1;
+        return n * factorial(n - 1);
+    }
 }
