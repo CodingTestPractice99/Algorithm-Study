b3fdc08eb353685aabe3030a97382c2ef7d11d81
ohjuyoungg <ejrm12@naver.com>
2024-08-19 12:23:23 +0900

    백준: 이항 계수 1 (240819)

--- study/src/juyoungoh/beakjoon/_11050.java
+++ study/src/juyoungoh/beakjoon/_11050.java
@@ -1,2 +1,38 @@
-package juyoungoh.beakjoon;public class _11050 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _11050 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        String s2[] = s.split(" ");
+
+        long n = Integer.parseInt(s2[0]);
+        long k = Integer.parseInt(s2[1]);
+        long n2 = n - k;
+
+        long a = 1;
+        long b = 1;
+        long c = 1;
+
+        a = getMultiply(n, a);
+        b = getMultiply(k, b);
+        c = getMultiply(n2, c);
+
+        long a2 = b * c;
+        System.out.println(a / a2);
+    }
+
+    private static long getMultiply(long n, long a) {
+        while (n != 0) {
+            a *= n;
+            n--;
+        }
+        return a;
+    }
 }
