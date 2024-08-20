4697fdb25a33d2167fc5c25d4e3345e7d05b2a32
ohjuyoungg <ejrm12@naver.com>
2024-08-20 12:52:27 +0900

    백준: 팩토리얼 2 (240820)

--- study/src/juyoungoh/beakjoon/_27433_1.java
+++ study/src/juyoungoh/beakjoon/_27433_1.java
@@ -1,2 +1,25 @@
-package juyoungoh.beakjoon;public class _27433_1 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _27433_1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Long.parseLong(br.readLine());
+        long answer = calculateFactorial(n);
+
+        System.out.println(answer);
+    }
+
+    public static long calculateFactorial(long n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
 }
