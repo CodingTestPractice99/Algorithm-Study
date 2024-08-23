bb463b8f9c73dab3cde483061e74b830f5d1f195
ohjuyoungg <ejrm12@naver.com>
2024-08-23 22:32:55 +0900

    백준: 문자열 집합 (240823)

---
+++ study/src/juyoungoh/beakjoon/_14425_1.java
@@ -0,0 +1,32 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class _14425_1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(br.readLine(), 0);
+        }
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            if (map.containsKey(br.readLine())) {
+                count++;
+            }
+        }
+        System.out.print(count);
+    }
+}
