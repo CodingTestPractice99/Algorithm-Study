f5143229ee07a79c1a981f0ba8281f2ca89b677d
ohjuyoungg <ejrm12@naver.com>
2024-08-23 16:16:28 +0900

    백준: 행렬 덧셈 (240823)

---
+++ study/src/juyoungoh/beakjoon/_2738_1.java
@@ -0,0 +1,35 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _2738_1 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int arr[][] = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                sb.append(arr[i][j] + Integer.parseInt(st.nextToken()) + " ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
