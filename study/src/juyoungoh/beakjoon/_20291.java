74c81148058898cc77c96382248036b2b4af8bf9
ohjuyoungg <ejrm12@naver.com>
2024-08-24 13:40:35 +0900

    백준: 파일 정리 (240824)

--- study/src/juyoungoh/beakjoon/_20291.java
+++ study/src/juyoungoh/beakjoon/_20291.java
@@ -1,2 +1,29 @@
-package juyoungoh.beakjoon;public class _20291 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class _20291 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Map<String, Integer> map = new TreeMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String str[] = br.readLine().split("\\.");
+            map.put(str[1], map.getOrDefault(str[1], 0) + 1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            sb.append(entry.getKey()).append(" ").append(entry.getValue()).append("\n");
+        }
+        System.out.print(sb);
+    }
 }
