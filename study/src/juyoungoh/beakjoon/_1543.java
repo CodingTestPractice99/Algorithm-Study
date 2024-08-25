e19945bfc15ba07e55642b625865776a4a01d889
ohjuyoungg <ejrm12@naver.com>
2024-08-25 15:09:02 +0900

    commit

--- study/src/juyoungoh/beakjoon/_1543.java
+++ study/src/juyoungoh/beakjoon/_1543.java
@@ -1,2 +1,22 @@
-package juyoungoh.beakjoon;public class _1543 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _1543 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String answer = br.readLine();
+
+        int count = 0;
+
+        for (int i = 0; i < str.length() / answer.length(); i++) {
+            if (str.substring(i, answer.length()).equals(answer)) {
+                count++;
+            }
+        }
+    }
 }
