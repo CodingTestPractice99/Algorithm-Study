129cc14e9cfa2abb82ae8ab0401892a94f59185e
ohjuyoungg <ejrm12@naver.com>
2024-09-03 14:09:04 +0900

    백준: 진법 변환 2 (240903)

---
+++ study/src/juyoungoh/beakjoon/_11005.java
@@ -0,0 +1,20 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _11005 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        String str = Integer.toString(a, b);
+        System.out.println(str.toUpperCase());
+    }
+}
