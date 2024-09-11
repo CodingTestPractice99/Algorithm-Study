8817e8b22bb14dbde19c95c393de64ea794e5019
ohjuyoungg <ejrm12@naver.com>
2024-09-11 17:48:57 +0900

    백준: 직사각형 별찍기 (240911)

---
+++ study/src/juyoungoh/programmers/lv1/_12969.java
@@ -0,0 +1,24 @@
+package juyoungoh.programmers.lv1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _12969 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
