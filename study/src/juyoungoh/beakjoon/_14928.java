d4ed20f03b9f9f7d26d54f07c4095652039803c0
ohjuyoungg <ejrm12@naver.com>
2024-09-13 18:17:31 +0900

    리트코드: 819. Most Common Word (240913)

--- study/src/juyoungoh/beakjoon/_14928.java
+++ study/src/juyoungoh/beakjoon/_14928.java
@@ -1,5 +1,20 @@
 package juyoungoh.beakjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class _14928 {
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String num = br.readLine();
+        long remainder = 0;
+
+        for (int i = 0; i < num.length(); i++) {
+            remainder = (remainder * 10 + (num.charAt(i) - '0')) % 20000303;
+        }
+        System.out.println(remainder);
+    }
 }
