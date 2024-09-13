f98ad66f9c9353e7a32dea4be5f157f10eacb93e
ohjuyoungg <ejrm12@naver.com>
2024-09-13 20:44:21 +0900

    백준: 큰 수 (BIG) (240913)

--- study/src/juyoungoh/beakjoon/_14928.java
+++ study/src/juyoungoh/beakjoon/_14928.java
@@ -10,7 +10,7 @@ public class _14928 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String num = br.readLine();
-        long remainder = 0;
+        int remainder = 0;
 
         for (int i = 0; i < num.length(); i++) {
             remainder = (remainder * 10 + (num.charAt(i) - '0')) % 20000303;
