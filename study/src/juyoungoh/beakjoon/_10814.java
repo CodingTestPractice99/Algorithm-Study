4ee1edf44f1b8f698d8be5a7c22889f284807b12
ohjuyoungg <ejrm12@naver.com>
2024-09-14 22:52:02 +0900

    백준: 나이순 정렬 (240914)

--- study/src/juyoungoh/beakjoon/_10814.java
+++ study/src/juyoungoh/beakjoon/_10814.java
@@ -21,17 +21,14 @@ public class _10814 {
             list.add(str);
         }
 
-        Collections.sort(list, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                String s1[] = o1.split(" ");
-                String s2[] = o2.split(" ");
+        Collections.sort(list, (o1, o2) -> {
+            String s1[] = o1.split(" ");
+            String s2[] = o2.split(" ");
 
-                int s1Age = Integer.parseInt(s1[0]);
-                int s2Age = Integer.parseInt(s2[0]);
+            int s1Age = Integer.parseInt(s1[0]);
+            int s2Age = Integer.parseInt(s2[0]);
 
-                return s1Age < s2Age ? -1 : (s1Age == s2Age ? 0 : 1);
-            }
+            return Integer.compare(s1Age, s2Age);
         });
 
         StringBuilder sb = new StringBuilder();
