ae1d1ef2b4d36e5a7371331f84756e87e5ad0629
ohjuyoungg <ejrm12@naver.com>
2024-09-14 22:49:06 +0900

    백준: 나이순 정렬 (240914)

--- study/src/juyoungoh/beakjoon/_10814.java
+++ study/src/juyoungoh/beakjoon/_10814.java
@@ -5,32 +5,39 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+
+
 
 public class _10814 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
         List<String> list = new ArrayList<>();
-
         for (int i = 0; i < n; i++) {
-            list.add(br.readLine());
+            String str = br.readLine();
+            list.add(str);
         }
 
-        for (int i = 0; i < list.size(); i++) {
-            list.get(i).split(" ");
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String s1[] = o1.split(" ");
+                String s2[] = o2.split(" ");
+
+                int s1Age = Integer.parseInt(s1[0]);
+                int s2Age = Integer.parseInt(s2[0]);
+
+                return s1Age < s2Age ? -1 : (s1Age == s2Age ? 0 : 1);
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : list) {
+            sb.append(s).append("\n");
         }
-
-
-
-
-
-
-
+        System.out.print(sb);
     }
-
 }
