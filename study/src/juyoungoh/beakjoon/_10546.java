f65ec1b48f841ac8e9608bcb52390c25966707fc
ohjuyoungg <ejrm12@naver.com>
2024-09-21 10:45:41 +0900

    백준: 배부른 마라토너 (240921)

--- study/src/juyoungoh/beakjoon/_10546.java
+++ study/src/juyoungoh/beakjoon/_10546.java
@@ -1,2 +1,33 @@
-package juyoungoh.beakjoon;public class _10546 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class _10546 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<String, Integer> map = new HashMap();
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 1) {
+            System.out.println(br.readLine());
+            return;
+        }
+
+        for (int i = 0; i < n + (n - 1); i++) {
+            String s = br.readLine();
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        for (Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() % 2 == 1) {
+                System.out.println(entry.getKey());
+            }
+        }
+    }
 }
