81d387b431009768e59f06d0d65032b4ae13cd5d
ohjuyoungg <ejrm12@naver.com>
2024-09-23 12:10:01 +0900

    백준: 16진수 (240923)

---
+++ study/src/juyoungoh/beakjoon/_1550.java
@@ -0,0 +1,15 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _1550 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        System.out.println(Integer.parseInt(str, 16));
+    }
+}
