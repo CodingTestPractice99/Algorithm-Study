14ea08c56e9925c38f246c76af700da62538e31e
ohjuyoungg <ejrm12@naver.com>
2024-10-03 09:52:33 +0900

    백준: 콰트로치즈피자 (241003)

--- study/src/juyoungoh/beakjoon/_27964.java
+++ study/src/juyoungoh/beakjoon/_27964.java
@@ -1,2 +1,30 @@
-package juyoungoh.beakjoon;public class _27964 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class _27964 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<String, Integer> map = new HashMap<>();
+
+        int n = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        String arr[] = str.split(" ");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].endsWith("Cheese")) {
+                map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            }
+        }
+        if (map.size() >= 4) {
+            System.out.println("yummy");
+        } else {
+            System.out.println("sad");
+        }
+    }
 }
