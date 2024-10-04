83e48da665bcc3bba28f5daabfc5b4c0e55ffabe
ohjuyoungg <ejrm12@naver.com>
2024-10-04 14:55:22 +0900

    백준: 라면 공식 (241004)

--- study/src/juyoungoh/beakjoon/_30007.java
+++ study/src/juyoungoh/beakjoon/_30007.java
@@ -1,2 +1,26 @@
-package juyoungoh.beakjoon;public class _30007 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _30007 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String arr[] = br.readLine().split(" ");
+            int arrInt[] = new int[arr.length];
+
+            for (int j = 0; j < arr.length; j++) {
+                arrInt[j] = Integer.parseInt(arr[j]);
+            }
+
+            int answer = (arrInt[0] * (arrInt[2] - 1)) + arrInt[1];
+            System.out.println(answer);
+        }
+    }
 }
