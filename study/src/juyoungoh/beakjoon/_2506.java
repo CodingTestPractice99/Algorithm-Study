abae2b21badbdefde81cbdbf9068a7d534c7875b
ohjuyoungg <ejrm12@naver.com>
2024-10-04 15:05:11 +0900

    백준: 점수계산 (241004):

--- study/src/juyoungoh/beakjoon/_2506.java
+++ study/src/juyoungoh/beakjoon/_2506.java
@@ -1,2 +1,29 @@
-package juyoungoh.beakjoon;public class _2506 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2506 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        String arr[] = br.readLine().split(" ");
+
+        int answer = 0;
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("1")) {
+                count++;
+                answer += count;
+            } else {
+                count = 0;
+            }
+        }
+        System.out.println(answer);
+    }
 }
