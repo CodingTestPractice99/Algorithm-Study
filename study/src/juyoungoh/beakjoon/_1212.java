d5790812a3bc92e7ffdc20a8e603a290538ae273
ohjuyoungg <ejrm12@naver.com>
2024-10-04 15:08:49 +0900

    백준: 8진수 2진수 (241004)

--- study/src/juyoungoh/beakjoon/_1212.java
+++ study/src/juyoungoh/beakjoon/_1212.java
@@ -1,2 +1,18 @@
-package juyoungoh.beakjoon;public class _1212 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _1212 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        int decimal = Integer.parseInt(str, 8);
+        String binary = Integer.toBinaryString(decimal);
+
+        System.out.println(binary);
+    }
 }
