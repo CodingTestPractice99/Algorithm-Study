db0df987a4e1e8e5236373bf8b292d70f8c04cda
ohjuyoungg <ejrm12@naver.com>
2024-10-04 21:43:39 +0900

    백준: 네 수 (241004)

--- study/src/juyoungoh/beakjoon/_10824.java
+++ study/src/juyoungoh/beakjoon/_10824.java
@@ -1,7 +1,22 @@
 package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class _10824 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        String arr[] = br.readLine().split(" ");
+
+        String a = arr[0] + arr[1];
+        String b = arr[2] + arr[3];
+
+        int aNum = Integer.parseInt(a);
+        int bNum = Integer.parseInt(b);
+
+        System.out.println(aNum + bNum);
     }
 }
