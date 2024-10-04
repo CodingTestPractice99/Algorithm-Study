3b6e71f7078b82c59c02de0277092156f9a07fed
ohjuyoungg <ejrm12@naver.com>
2024-10-04 15:17:59 +0900

    백준: 나는 요리사다 (241004)

--- study/src/juyoungoh/beakjoon/_2953.java
+++ study/src/juyoungoh/beakjoon/_2953.java
@@ -1,2 +1,29 @@
-package juyoungoh.beakjoon;public class _2953 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2953 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int max = 0;
+        int index = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            String arr[] = br.readLine().split(" ");
+
+            int sum = 0;
+            for (int j = 0; j < arr.length; j++) {
+                sum += Integer.parseInt(arr[j]);
+            }
+            if (max < sum) {
+                index = i;
+                max = sum;
+            }
+        }
+        System.out.println(index + " " + max);
+    }
 }
