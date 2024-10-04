cbe80489314f0cf80e16662d012eb1437334c942
ohjuyoungg <ejrm12@naver.com>
2024-10-04 15:01:48 +0900

    백준: 홀수일까 짝수일까 (241004)

--- study/src/juyoungoh/beakjoon/_5988.java
+++ study/src/juyoungoh/beakjoon/_5988.java
@@ -1,2 +1,28 @@
-package juyoungoh.beakjoon;public class _5988 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class _5988 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        BigInteger bigInteger = new BigInteger("2");
+        BigInteger zero = new BigInteger("0");
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            BigInteger input = new BigInteger(str);
+
+            if (input.remainder(bigInteger).equals(zero)) {
+                System.out.println("even");
+            } else {
+                System.out.println("odd");
+            }
+        }
+    }
 }
