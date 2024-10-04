8afda99f288b9f67fcac82ea5cb7aa4bc6b25c23
ohjuyoungg <ejrm12@naver.com>
2024-10-04 14:50:45 +0900

    백준: 자동완성 (241004)

--- study/src/juyoungoh/beakjoon/_24883.java
+++ study/src/juyoungoh/beakjoon/_24883.java
@@ -1,2 +1,20 @@
-package juyoungoh.beakjoon;public class _24883 {
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _24883 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        if (str.equals("n") || str.equals("N")) {
+            System.out.println("Naver D2");
+        } else {
+            System.out.println("Naver Whale");
+        }
+    }
 }
