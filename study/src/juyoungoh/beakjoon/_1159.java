584452555820c57476f25a4d5c3174593e17dee6
ohjuyoungg <ejrm12@naver.com>
2024-10-04 21:41:37 +0900

    백준: 농구 경기 (241004)

--- study/src/juyoungoh/beakjoon/_1159.java
+++ study/src/juyoungoh/beakjoon/_1159.java
@@ -1,7 +1,40 @@
 package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class _1159 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a[] = new int[26];
 
+        int n = Integer.parseInt(br.readLine());
+
+        String answer = "";
+
+        for (int i = 0; i < n; i++) {
+            char c[] = br.readLine().toCharArray();
+            a[c[0] - 97] += 1;
+        }
+
+        char alphabet[] = new char[26];
+
+        for (int i = 0; i < alphabet.length; i++) {
+            alphabet[i] = (char) ('a' + i);
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] >= 5) {
+                answer += alphabet[i];
+            }
+        }
+
+        if (answer.isBlank()) {
+            System.out.println("PREDAJA");
+        } else {
+            System.out.println(answer);
+        }
     }
 }
