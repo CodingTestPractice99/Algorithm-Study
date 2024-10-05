6d076c2711f7f2625728913635733c65f62be40f
ohjuyoungg <ejrm12@naver.com>
2024-10-05 11:46:07 +0900

    리트코드: 92. Reverse Linked List II (241005)

---
+++ study/src/juyoungoh/leetcode/_92_ReverseLinkedListII.java
@@ -0,0 +1,33 @@
+package juyoungoh.leetcode;
+
+import juyoungoh.leetcode._2_AddTwoNumbers_1.ListNode;
+
+public class _92_ReverseLinkedListII {
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        // 예외 처리
+        if (head == null) {
+            return null;
+        }
+        // 임시 노드 선언
+        ListNode root = new ListNode(0);
+        // 임시 노드 다음으로 노드 시작
+        root.next = head;
+        // 임시 노드부터 시작해 변경 필요한 위치 앞으로 이동
+        ListNode start = root;
+        for (int i = 0; i < left - 1; i++) {
+            start = start.next;
+        }
+        // 변경이 필요한 마지막 위치 선언
+        ListNode end = start.next;
+        // right - left 만큼 위치 변경 진행
+        for (int i = 0; i < right - left; i++) {
+            ListNode tmp = start.next;
+            start.next = end.next;
+            end.next = end.next.next;
+            start.next.next = tmp;
+        }
+        // 첫 번째 노드는 임시 노드이므로 그 다음부터 결과로 리턴
+        return root.next;
+    }
+}
