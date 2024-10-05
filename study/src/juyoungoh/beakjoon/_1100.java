c30cdbde8e76e996dd0bc263c0421bf31d390048
ohjuyoungg <ejrm12@naver.com>
2024-10-05 14:54:05 +0900

    백준: 하얀 칸 (241005)

--- study/src/juyoungoh/beakjoon/_1100.java
+++ study/src/juyoungoh/beakjoon/_1100.java
@@ -1,8 +1,26 @@
 package juyoungoh.beakjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class _1100 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int answer = 0;
+
+        for (int i = 1; i <= 8; i++) {
+            String str[] = br.readLine().split("");
+            for (int j = 0; j < str.length; j++) {
+                if (str[j].equals("F") && i % 2 == 1 && j % 2 == 0) {
+                    answer++;
+                } else if (str[j].equals("F") && i % 2 == 0 && j % 2 == 1) {
+                    answer++;
+                }
+            }
+        }
+        System.out.println(answer);
     }
 }
