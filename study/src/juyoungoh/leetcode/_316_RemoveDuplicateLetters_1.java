882f1c99b621a235d024892d9a64e047637ca5a3
ohjuyoungg <ejrm12@naver.com>
2024-10-11 10:51:44 +0900

    리트코드: 316. Remove Duplicate Letters (241011)

--- study/src/juyoungoh/leetcode/_316_RemoveDuplicateLetters_1.java
+++ study/src/juyoungoh/leetcode/_316_RemoveDuplicateLetters_1.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class _316_RemoveDuplicateLetters_1 {
+
     public String removeDuplicateLetters(String s) {
         // 문자 개수를 계산해서 담아둘 변수 선언
         Map<Character, Integer> counter = new HashMap<>();
@@ -22,7 +23,25 @@ public class _316_RemoveDuplicateLetters_1 {
         for (char c : s.toCharArray()) {
             // 현재 처리하는 문자는 카운터에서 -1 처리
             counter.put(c, counter.get(c) - 1);
+            // 이미 처리한 문자는 건너뛴다.
+            if (seen.get(c) != null && seen.get(c) == true) {
+                continue;
+            }
+            // 스택에 있는 문자가 현재 문자보다 더 뒤에 붙여야할 문자라면 스택에서 제거하고 처리하지 않은걸로 표시
+            while (!stack.isEmpty() && stack.peek() > c && counter.get(stack.peek()) > 0) {
+                seen.put(stack.pop(), false);
+            }
+            // 현재 문자를 스택에 삽입
+            stack.push(c);
+            // 현재 문자를 처리한 걸로 선언
+            seen.put(c, true);
         }
-        return "";
+
+        // 스택에 있는 문자를 큐 순서대로 추출하여 리턴
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pollLast());
+        }
+        return sb.toString();
     }
 }
