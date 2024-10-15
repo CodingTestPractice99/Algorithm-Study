1e466c03e4d88d1438e90af7173fe3aba4a8ea74
ohjuyoungg <ejrm12@naver.com>
2024-10-15 15:58:28 +0900

    백준: 줄세우기 (241015)

--- study/src/juyoungoh/beakjoon/_10431.java
+++ study/src/juyoungoh/beakjoon/_10431.java
@@ -3,7 +3,6 @@ package juyoungoh.beakjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.time.Period;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
