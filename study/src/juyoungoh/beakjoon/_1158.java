ee36e70c8579a112176422091812050072203f0d
ohjuyoungg <ejrm12@naver.com>
2024-10-17 15:56:25 +0900

    백준: 요세푸스 문제 (241017)

--- study/src/juyoungoh/beakjoon/_1158.java
+++ study/src/juyoungoh/beakjoon/_1158.java
@@ -3,31 +3,36 @@ package juyoungoh.beakjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class _1158 {
 
-    int[] q;
-    int front = 0, rear = -1, len = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String arr[] = br.readLine().split(" ");
 
-        int num = Integer.parseInt(arr[0]);
-        int count = Integer.parseInt(arr[1]);
+        int n = Integer.parseInt(arr[0]);
+        int k = Integer.parseInt(arr[1]);
 
-        int answer[] = new int[num];
-        int number[] = new int[num];
+        Queue<Integer> queue = new ArrayDeque<>();
 
-        for (int i = 0; i < num; i++) {
-            number[i] = i + 1;
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
         }
 
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
 
-
-
-
-
+        while (queue.size() > 1) {
+            for (int i = 0; i < k - 1; i++) {
+                queue.offer(queue.poll());
+            }
+            sb.append(queue.poll()).append(", ");
+        }
+        sb.append(queue.poll()).append(">");
+        System.out.print(sb.toString());
     }
-
 }
