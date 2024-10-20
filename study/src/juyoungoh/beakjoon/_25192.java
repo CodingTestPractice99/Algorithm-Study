11430e6b4e8281ffd253412d5bbc79de170ce466
ohjuyoungg <ejrm12@naver.com>
2024-10-20 17:03:28 +0900

    백준: 인사성 밝은 곰곰이 (241020)

--- study/src/juyoungoh/beakjoon/_25192.java
+++ study/src/juyoungoh/beakjoon/_25192.java
@@ -3,7 +3,8 @@ package juyoungoh.beakjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class _25192 {
@@ -12,18 +13,19 @@ public class _25192 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        ArrayList<String> strList = new ArrayList<>();
+        Set<String> set = new HashSet<>();
+
         int count = 0;
 
         for (int i = 0; i < n; i++) {
-             strList.add(br.readLine());
-            if (strList.get(i).equals("ENTER")) {
-                
-            }
-        }
-
-        for (int i = 0; i < strList.size(); i++) {
-            if (!(strList.get(i).equals("ENTER"))) {
+            String str = br.readLine();
+            if (str.equals("ENTER")) {
+                set.clear();
+            } else {
+                if (set.contains(str)) {
+                    continue;
+                }
+                set.add(str);
                 count++;
             }
         }
