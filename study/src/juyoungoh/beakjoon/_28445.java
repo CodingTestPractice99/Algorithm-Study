df4c704cf243f57bc016936fcc229c0df71ffb68
ohjuyoungg <ejrm12@naver.com>
2024-10-23 15:00:51 +0900

    백준: 알록달록 앵무새 (241023)

--- study/src/juyoungoh/beakjoon/_28445.java
+++ study/src/juyoungoh/beakjoon/_28445.java
@@ -3,22 +3,37 @@ package juyoungoh.beakjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class _28445 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        String dad[] = br.readLine().split(" ");
+        String mom[] = br.readLine().split(" ");
 
-        String strA1 = st.nextToken();
-        String strA2 = st.nextToken();
+        Set<String> set = new TreeSet<>();
 
-        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                set.add(dad[i] + " " + dad[i]);
+                set.add(dad[i] + " " + dad[j]);
+                set.add(mom[i] + " " + mom[i]);
+                set.add(mom[i] + " " + mom[j]);
+                set.add(dad[i] + " " + mom[j]);
+                set.add(mom[i] + " " + dad[j]);
+            }
+        }
 
-        String strB1 = st.nextToken();
-        String str = "";
+        for (String s : set) {
+            System.out.println(s);
+        }
     }
-
 }
