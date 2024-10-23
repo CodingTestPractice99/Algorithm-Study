9e0721da482fb6c2ff142a7ba944d1f72dd9c010
ohjuyoungg <ejrm12@naver.com>
2024-10-23 15:01:17 +0900

    백준: 알록달록 앵무새 (241023)

--- study/src/juyoungoh/beakjoon/_28445.java
+++ study/src/juyoungoh/beakjoon/_28445.java
@@ -3,11 +3,6 @@ package juyoungoh.beakjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeSet;
 
