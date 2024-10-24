c202540550fcb2a08c79884d935b65f738db114d
ohjuyoungg <ejrm12@naver.com>
2024-10-24 14:48:06 +0900

    백준: 멘토와 멘티 (241024)

--- study/src/juyoungoh/beakjoon/_26265.java
+++ study/src/juyoungoh/beakjoon/_26265.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map.Entry;
 import java.util.Set;
 
 public class _26265 {
