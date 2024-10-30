9bc8d418217def3a566cda4883162c0446036769
ohjuyoungg <ejrm12@naver.com>
2024-10-30 18:30:18 +0900

    백준: 팬들에게 둘러싸인 홍준 (241030)

--- study/src/juyoungoh/beakjoon/_14581.java
+++ study/src/juyoungoh/beakjoon/_14581.java
@@ -8,7 +8,6 @@ public class _14581 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String str = br.readLine();
 
         System.out.println(":fan::fan::fan:");
