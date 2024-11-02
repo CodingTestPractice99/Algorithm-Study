4f534ea37e49a6610904419077bdd218339f4196
ohjuyoungg <ejrm12@naver.com>
2024-11-02 12:36:20 +0900

    백준: 홀수일까 짝수일까 (241102)

--- study/src/juyoungoh/beakjoon/_5988.java
+++ study/src/juyoungoh/beakjoon/_5988.java
@@ -9,7 +9,6 @@ public class _5988 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
         BigInteger bigInteger = new BigInteger("2");
         BigInteger zero = new BigInteger("0");
