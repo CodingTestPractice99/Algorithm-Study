c4f07498ab3cb570b6509667f9493cc448b0541f
ohjuyoungg <ejrm12@naver.com>
2024-11-03 16:55:05 +0900

    백준: 점수계산 (241103)

--- study/src/juyoungoh/beakjoon/_2506.java
+++ study/src/juyoungoh/beakjoon/_2506.java
@@ -8,7 +8,6 @@ public class _2506 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
 
         String arr[] = br.readLine().split(" ");
