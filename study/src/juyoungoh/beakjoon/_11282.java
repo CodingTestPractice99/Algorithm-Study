da97e5a3d4562dda4eb23465de81e4267f613484
ohjuyoungg <ejrm12@naver.com>
2024-11-04 16:00:46 +0900

    백준: 한글 (241104)

---
+++ study/src/juyoungoh/beakjoon/_11282.java
@@ -0,0 +1,18 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _11282 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        n += 44031;
+        char c = (char) n;
+        System.out.println(c);
+    }
+}
