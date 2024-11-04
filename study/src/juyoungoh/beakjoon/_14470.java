ba6d3aacdb47a7604f1ff913f3ad6abc628804f9
ohjuyoungg <ejrm12@naver.com>
2024-11-04 15:57:56 +0900

    백준: 치킨댄스를 추는 곰곰이를 본 임스 2 (241104)

---
+++ study/src/juyoungoh/beakjoon/_14470.java
@@ -0,0 +1,20 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _14470 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        int e = Integer.parseInt(br.readLine());
+
+
+    }
+}
---
+++ study/src/juyoungoh/beakjoon/_26068.java
@@ -0,0 +1,27 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _26068 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            String arr[] = str.split("-");
+            int day = Integer.parseInt(arr[1]);
+            if (day <= 90) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
