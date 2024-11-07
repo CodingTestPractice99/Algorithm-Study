7e0764dc471042cfb936eb72039e04d27e4d6755
ohjuyoungg <ejrm12@naver.com>
2024-11-07 09:43:42 +0900

    백준: 8진수 2진수 (241107)

--- study/src/juyoungoh/beakjoon/_1212.java
+++ study/src/juyoungoh/beakjoon/_1212.java
@@ -8,11 +8,23 @@ public class _1212 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         String str = br.readLine();
-        int decimal = Integer.parseInt(str, 8);
-        String binary = Integer.toBinaryString(decimal);
 
-        System.out.println(binary);
+        for (int i = 0; i < str.length(); i++) {
+            int t = str.charAt(i) - '0';
+            String s = Integer.toBinaryString(t);
+            if (s.length() == 3) {
+                sb.append(s);
+            } else if (s.length() == 2 && i != 0) {
+                sb.append("0" + s);
+            } else if (s.length() == 1 && i != 0) {
+                sb.append("00" + s);
+            } else {
+                sb.append(s);
+            }
+        }
+        System.out.println(sb.toString());
     }
 }
