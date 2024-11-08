2a5e1638a50e232864dbec348596b409946bd24e
ohjuyoungg <ejrm12@naver.com>
2024-11-08 10:15:11 +0900

    백준: 2진수 8진수 (241108)

--- study/src/juyoungoh/beakjoon/_1373_1.java
+++ study/src/juyoungoh/beakjoon/_1373_1.java
@@ -14,7 +14,7 @@ public class _1373_1 {
         // 형변환을 해주면서 옆에 2를 적어주면 10진수로 바꿔준다.
         BigInteger N = new BigInteger(S, 2);
 
-        // BigInteger.toString이 String으로 형변환 해주는 것이고, 옆에 괄호안에 원하는 진수를 적어주면 된다.
+        // BigInteger.toString 이 String 으로 형변환 해주는 것이고, 옆에 괄호안에 원하는 진수를 적어주면 된다.
         String result = N.toString(8);
 
         System.out.println(result);
