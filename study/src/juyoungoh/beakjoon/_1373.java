71049992e8151b98b43578e438bef8332e5ec4e4
ohjuyoungg <ejrm12@naver.com>
2024-11-08 09:54:52 +0900

    백준: 2진수 8진수 (241108)

--- study/src/juyoungoh/beakjoon/_1373.java
+++ study/src/juyoungoh/beakjoon/_1373.java
@@ -9,7 +9,20 @@ public class _1373 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        StringBuilder sb = new StringBuilder();
         String str = br.readLine();
 
+        int padding = 3 - (str.length() % 3);
+        if (padding != 3) {
+            str = "0".repeat(padding) + str;
+        }
+
+        for (int i = 0; i < str.length(); i += 3) {
+            String chunk = str.substring(i, i + 3);
+            int octalDigit = Integer.parseInt(chunk, 2);
+            sb.append(octalDigit);
+        }
+
+        System.out.println(sb.toString());
     }
 }
