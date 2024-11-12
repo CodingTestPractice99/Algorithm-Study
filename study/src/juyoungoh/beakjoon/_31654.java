392d421c0234071fe46554548ea3f6ff900ea9f8
ohjuyoungg <ejrm12@naver.com>
2024-11-12 13:56:53 +0900

    백준: Adding Trouble (241112)

---
+++ study/src/juyoungoh/beakjoon/_31654.java
@@ -0,0 +1,24 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _31654 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String arr[] = br.readLine().split(" ");
+
+        int a = Integer.parseInt(arr[0]);
+        int b = Integer.parseInt(arr[1]);
+        int c = Integer.parseInt(arr[2]);
+
+        if ((a + b) == c) {
+            System.out.println("correct!");
+        } else {
+            System.out.println("wrong!");
+        }
+    }
+}
