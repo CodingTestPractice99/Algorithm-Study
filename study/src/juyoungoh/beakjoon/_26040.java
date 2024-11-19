d8e53dcbbf543b6f0231269f3c78c80950e75326
ohjuyoungg <ejrm12@naver.com>
2024-11-19 11:44:31 +0900

    백준: 특정 대문자를 소문자로 바꾸기 (241119)

---
+++ study/src/juyoungoh/beakjoon/_26040.java
@@ -0,0 +1,30 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _26040 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str[] = br.readLine().split("");
+        String arr[] = br.readLine().split(" ");
+
+        String string = "";
+        for (int i = 0; i < arr.length; i++) {
+            string += arr[i];
+        }
+
+        String answer = "";
+        for (int i = 0; i < str.length; i++) {
+            if (string.contains(str[i])) {
+                answer += str[i].toLowerCase();
+            } else {
+                answer += str[i];
+            }
+        }
+        System.out.println(answer);
+    }
+}
