bb81e646d9bac0823628de6e621f65a5ccc1def9
ohjuyoungg <ejrm12@naver.com>
2024-11-21 15:18:58 +0900

    백준: 좋다 (241121)

--- study/src/juyoungoh/beakjoon/_1253.java
+++ study/src/juyoungoh/beakjoon/_1253.java
@@ -9,37 +9,40 @@ import java.util.Set;
 
 public class _1253 {
 
-    // 어떤 수가 다른 수 두 개의 합으로 나타낼 수 있다
-
-    /**
-     * 5 -1 0 1 2 3
-     */
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
         String arr[] = br.readLine().split(" ");
-        int num[] = new int[arr.length];
+        int numArr[] = new int[arr.length];
         Set<Integer> set = new HashSet<>();
 
         for (int i = 0; i < arr.length; i++) {
-            num[i] = Integer.parseInt(arr[i]);
+            numArr[i] = Integer.parseInt(arr[i]);
             set.add(Integer.parseInt(arr[i]));
         }
-        Arrays.sort(num);
 
+        Arrays.sort(numArr);
+        int left = 0;
+        int right = n - 1;
         int index = n - 1;
-        for (int i = 0; i < num.length; i++) {
-            for (int j = 1; j < num.length; j++) {
-                if (set.contains(num[i] + num[j]) && i != j && i != index && j != index && index >= 0) {
-                    set.remove(num[i] + num[j]);
-                }
-            }
-            index--;
-        }
+        int a = 0;
+        int b = 0;
 
+        while (left < right) {
+            long sum = numArr[left] + numArr[right];
+
+            if (sum == 0) {
+                a = numArr[left];
+                b = numArr[right];
+                break;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
         System.out.println(n - set.size());
     }
 }
