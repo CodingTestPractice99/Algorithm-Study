3ef1534365248cb32aacc9e88b3b9e404c291942
ohjuyoungg <ejrm12@naver.com>
2024-11-30 18:16:30 +0900

    백준: 엘리스 트랙 매칭 (241130)

--- study/src/juyoungoh/beakjoon/_31428.java
+++ study/src/juyoungoh/beakjoon/_31428.java
@@ -8,7 +8,6 @@ public class _31428 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
         String arr[] = br.readLine().split(" ");
         String str = br.readLine();
