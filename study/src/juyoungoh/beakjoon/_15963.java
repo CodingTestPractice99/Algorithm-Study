e822d1df85fb5817ee3cae517bdc042e809ad4d7
ohjuyoungg <ejrm12@naver.com>
2024-12-01 14:04:25 +0900

    백준: CASIO (241201)

---
+++ study/src/juyoungoh/beakjoon/_15963.java
@@ -0,0 +1,24 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _15963 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String arr[] = br.readLine().split(" ");
+
+        int n = Integer.parseInt(arr[0]);
+        int m = Integer.parseInt(arr[1]);
+
+        if (n == m) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+
+}
