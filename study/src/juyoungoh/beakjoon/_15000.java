25a157d21922babb673c3834bf2237c1bd6ddf84
ohjuyoungg <ejrm12@naver.com>
2024-12-01 14:12:45 +0900

    백준: CAPS (241201)

---
+++ study/src/juyoungoh/beakjoon/_15000.java
@@ -0,0 +1,16 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _15000 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        String answer = str.toUpperCase();
+        System.out.println(answer);
+    }
+}
