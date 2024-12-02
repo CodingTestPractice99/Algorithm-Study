d154e878550c60136d663b04eaa4a51a336c57a8
ohjuyoungg <ejrm12@naver.com>
2024-12-02 16:50:03 +0900

    백준: 2023은 무엇이 특별할까? (241202)

--- study/src/juyoungoh/beakjoon/_31090.java
+++ study/src/juyoungoh/beakjoon/_31090.java
@@ -8,6 +8,7 @@ public class _31090 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
