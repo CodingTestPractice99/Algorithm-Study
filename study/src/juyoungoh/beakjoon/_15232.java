4d44d4106ad7ca1e7cc576ba5b234b8485c073a2
ohjuyoungg <ejrm12@naver.com>
2024-12-03 14:17:09 +0900

    백준: Rectangles (241203)

---
+++ study/src/juyoungoh/beakjoon/_15232.java
@@ -0,0 +1,23 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _15232 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
