e386d3d423effd3e7ed58ab9610d86d2d68a5518
ohjuyoungg <ejrm12@naver.com>
2024-12-03 14:01:55 +0900

    백준: SciComLove (241203)

---
+++ study/src/juyoungoh/beakjoon/_21598.java
@@ -0,0 +1,18 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _21598 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("SciComLove");
+        }
+    }
+}
