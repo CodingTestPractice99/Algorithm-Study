6be13b68c5e5250c273edb457edab028467366ec
ohjuyoungg <ejrm12@naver.com>
2024-12-05 12:53:06 +0900

    백준: 짝수를 찾아라 (241205)

--- study/src/juyoungoh/beakjoon/_3058.java
+++ study/src/juyoungoh/beakjoon/_3058.java
@@ -9,7 +9,6 @@ public class _3058 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
