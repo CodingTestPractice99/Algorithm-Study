943baeb3d85338c476f16a373448e77af5770dac
ohjuyoungg <ejrm12@naver.com>
2024-12-21 22:10:09 +0900

    백준: Who is in the middle? (241221)

--- study/src/juyoungoh/beakjoon/_6840.java
+++ study/src/juyoungoh/beakjoon/_6840.java
@@ -9,7 +9,6 @@ public class _6840 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int arr[] = new int[3];
 
         for (int i = 0; i < 3; i++) {
