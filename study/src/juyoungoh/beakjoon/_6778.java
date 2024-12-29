09a1fb1870a15da501d3b258034eb1131a48e708
ohjuyoungg <ejrm12@naver.com>
2024-12-29 19:09:03 +0900

    백준: Which Alien? (241229)

--- study/src/juyoungoh/beakjoon/_6778.java
+++ study/src/juyoungoh/beakjoon/_6778.java
@@ -8,7 +8,6 @@ public class _6778 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int a = Integer.parseInt(br.readLine());
         int b = Integer.parseInt(br.readLine());
 
