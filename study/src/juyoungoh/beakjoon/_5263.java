5d2beff2ebd55168adc8db652ee398997105e52a
ohjuyoungg <ejrm12@naver.com>
2025-01-15 14:59:48 +0900

    백준: samba (250115)

---
+++ study/src/juyoungoh/beakjoon/_5263.java
@@ -0,0 +1,32 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class _5263 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s[] = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+
+        for (Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() % m != 0) {
+                System.out.println(entry.getKey());
+                break;
+            }
+        }
+    }
+}
