8aad5d1df68d4efad2446c82e40c8f3dfe297e5e
ohjuyoungg <ejrm12@naver.com>
2025-01-25 13:48:34 +0900

    백준: 알고리즘 수업 - 알고리즘의 수행 시간 3 (250125)

---
+++ study/src/juyoungoh/beakjoon/_24264.java
@@ -0,0 +1,17 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _24264 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Long.parseLong(br.readLine());
+
+        System.out.println(n * n);
+        System.out.println(2);
+    }
+}
