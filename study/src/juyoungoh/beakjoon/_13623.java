a35ae8cdb7baf64cea7ad6655726a31930bb5b5b
ohjuyoungg <ejrm12@naver.com>
2025-01-31 15:31:33 +0900

    백준: Zero or One (250131)

---
+++ study/src/juyoungoh/beakjoon/_13623.java
@@ -0,0 +1,25 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _13623 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] s = br.readLine().split(" ");
+        
+        if (s[0].equals(s[1]) && s[1].equals(s[2])) {
+            System.out.println("*");
+            return;
+        } else if (s[0].equals(s[1])) {
+            System.out.println("C");
+        } else if (s[1].equals(s[2])) {
+            System.out.println("A");
+        } else {
+            System.out.println("B");
+        }
+    }
+}
