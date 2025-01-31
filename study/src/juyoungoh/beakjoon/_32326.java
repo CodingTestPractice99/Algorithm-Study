6e427e006cc041999858c9315e119beb86835d42
ohjuyoungg <ejrm12@naver.com>
2025-01-31 16:23:12 +0900

    백준: Conveyor Belt Sushi (250131)

---
+++ study/src/juyoungoh/beakjoon/_32326.java
@@ -0,0 +1,18 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _32326 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int r = Integer.parseInt(br.readLine()) * 3;
+        int g = Integer.parseInt(br.readLine()) * 4;
+        int b = Integer.parseInt(br.readLine()) * 5;
+
+        System.out.println(r + g + b);
+    }
+}
