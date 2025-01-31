e44fc1d1cf52895bb014c0a8d5d694cafafdb140
ohjuyoungg <ejrm12@naver.com>
2025-01-31 15:04:07 +0900

    백준: 와글와글 숭고한 (250131)

---
+++ study/src/juyoungoh/beakjoon/_17388.java
@@ -0,0 +1,36 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _17388 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s[] = br.readLine().split(" ");
+
+        int sum = 0;
+
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
+        int c = Integer.parseInt(s[2]);
+
+        for (int i = 0; i < s.length; i++) {
+            sum += Integer.parseInt(s[i]);
+        }
+
+        if (sum >= 100) {
+            System.out.println("OK");
+        } else {
+            if (a < b && a < c) {
+                System.out.println("Soongsil");
+            } else if (b < a && b < c) {
+                System.out.println("Korea");
+            } else {
+                System.out.println("Hanyang");
+            }
+        }
+    }
+}
