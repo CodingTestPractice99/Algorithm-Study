d9cc88abe4eafb4c08f1269c8b68eb797704c681
ohjuyoungg <ejrm12@naver.com>
2025-01-31 15:45:44 +0900

    백준: Java Warriors (250131)

---
+++ study/src/juyoungoh/beakjoon/_30328.java
@@ -0,0 +1,16 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _30328 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(n * 4000);
+    }
+}
