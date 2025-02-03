e702e1aaf33a885ba557cc0672e3b2d533f8374e
ohjuyoungg <ejrm12@naver.com>
2025-02-03 13:51:41 +0900

    백준: 時間 (Hour) (250203)

---
+++ study/src/juyoungoh/beakjoon/_27327.java
@@ -0,0 +1,15 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _27327 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n * 24);
+    }
+}
