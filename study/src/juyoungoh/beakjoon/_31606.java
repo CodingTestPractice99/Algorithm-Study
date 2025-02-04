2934514f56cb1aeb75b3f801742b2105169526bb
ohjuyoungg <ejrm12@naver.com>
2025-02-04 17:11:25 +0900

    백준: 果物 (Fruit) (250204)

---
+++ study/src/juyoungoh/beakjoon/_31606.java
@@ -0,0 +1,17 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _31606 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        System.out.println(n + m + 3);
+    }
+}
