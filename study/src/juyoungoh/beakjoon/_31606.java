ec47bf5544f4885f7685fdb533a93da3b835d343
ohjuyoungg <ejrm12@naver.com>
2025-02-05 20:24:17 +0900

    백준: 果物 (Fruit) (250205)

--- study/src/juyoungoh/beakjoon/_31606.java
+++ study/src/juyoungoh/beakjoon/_31606.java
@@ -8,7 +8,6 @@ public class _31606 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
 
