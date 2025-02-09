6e9c564244a2d227cdc60fc641276ddc3b8da237
ohjuyoungg <ejrm12@naver.com>
2025-02-09 23:07:32 +0900

    백준: Final Price (250209)

--- study/src/juyoungoh/beakjoon/_28224.java
+++ study/src/juyoungoh/beakjoon/_28224.java
@@ -8,7 +8,6 @@ public class _28224 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
         int sum = 0;
 
