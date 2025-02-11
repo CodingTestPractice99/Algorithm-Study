9be4de183b67bdb0eb17318a14f73337335314a3
ohjuyoungg <ejrm12@naver.com>
2025-02-11 22:48:46 +0900

    백준: Java Warriors (250211)

--- study/src/juyoungoh/beakjoon/_30328.java
+++ study/src/juyoungoh/beakjoon/_30328.java
@@ -8,7 +8,6 @@ public class _30328 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
 
         System.out.println(n * 4000);
