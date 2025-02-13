dbc7d1d37c488768773182b7b531c973775d39d3
ohjuyoungg <ejrm12@naver.com>
2025-02-13 22:56:51 +0900

    백준: 身長 (Height) (250213)

--- study/src/juyoungoh/beakjoon/_24086.java
+++ study/src/juyoungoh/beakjoon/_24086.java
@@ -8,7 +8,6 @@ public class _24086 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
 
