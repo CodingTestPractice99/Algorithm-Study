5c467a94f6cf7dfba33c2b6b83a1c1b4a96ae0c4
ohjuyoungg <ejrm12@naver.com>
2025-02-14 14:15:08 +0900

    백준: 飴の袋詰め (Drops Packing) (250214)

--- study/src/juyoungoh/beakjoon/_31610.java
+++ study/src/juyoungoh/beakjoon/_31610.java
@@ -8,7 +8,6 @@ public class _31610 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
         int z = Integer.parseInt(br.readLine());
