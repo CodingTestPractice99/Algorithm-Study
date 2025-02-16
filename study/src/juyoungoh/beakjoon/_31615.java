e308f9dc7310c6465a55389a78fc1ead4df86012
ohjuyoungg <ejrm12@naver.com>
2025-02-16 21:18:29 +0900

    백준: 桁 (Digit) (250216)

--- study/src/juyoungoh/beakjoon/_31615.java
+++ study/src/juyoungoh/beakjoon/_31615.java
@@ -8,7 +8,6 @@ public class _31615 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
 
