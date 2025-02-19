6819bd1a1480137a13ddadb955eaf7efab70c69f
ohjuyoungg <ejrm12@naver.com>
2025-02-19 12:11:59 +0900

    백준: Mathematics (250219)

---
+++ study/src/juyoungoh/beakjoon/_26545.java
@@ -0,0 +1,20 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _26545 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += Integer.parseInt(br.readLine());
+        }
+        System.out.println(sum);
+    }
+}
