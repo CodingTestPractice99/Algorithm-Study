1160ab36a239a29a012c72fdc21ca7f71aad7bba
ohjuyoungg <ejrm12@naver.com>
2025-02-24 20:14:52 +0900

    백준: 立方体 (Cube) (250224)

--- study/src/juyoungoh/beakjoon/_24082.java
+++ study/src/juyoungoh/beakjoon/_24082.java
@@ -8,7 +8,6 @@ public class _24082 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         long n = Long.parseLong(br.readLine());
 
         System.out.println(n * n * n);
