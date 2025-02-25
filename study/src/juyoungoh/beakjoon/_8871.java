9d3e9d59a7acc3a153ba0904a7013a54306b628f
ohjuyoungg <ejrm12@naver.com>
2025-02-25 14:04:01 +0900

    백준: Zadanie próbne 2 (250225)

---
+++ study/src/juyoungoh/beakjoon/_8871.java
@@ -0,0 +1,19 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _8871 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int min = 2 + (3 * n);
+        int max = 3 + (3 * n);
+
+        System.out.println(min + " " + max);
+    }
+}
