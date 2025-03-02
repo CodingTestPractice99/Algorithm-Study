e57239c8b49a44e5168924b35e6bf84cf73bc4e6
ohjuyoungg <ejrm12@naver.com>
2025-03-02 20:33:49 +0900

    백준: HI-ARC=? (250302)

---
+++ study/src/juyoungoh/beakjoon/_28444.java
@@ -0,0 +1,24 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _28444 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str[] = br.readLine().split(" ");
+
+        int h = Integer.parseInt(str[0]);
+        int i = Integer.parseInt(str[1]);
+        int a = Integer.parseInt(str[2]);
+        int r = Integer.parseInt(str[3]);
+        int c = Integer.parseInt(str[4]);
+
+        int num1 = h * i;
+        int num2 = a * r * c;
+
+        System.out.println(num1 - num2);
+    }
+}
