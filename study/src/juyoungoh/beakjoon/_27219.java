368fafc05d4d14af3e456a111efac5fc329e2415
ohjuyoungg <ejrm12@naver.com>
2025-03-04 14:51:34 +0900

    백준: Робинзон Крузо (250304)

---
+++ study/src/juyoungoh/beakjoon/_27219.java
@@ -0,0 +1,28 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _27219 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int five = n / 5;
+        int one = n % 5;
+
+        String answer = "";
+
+        for (int i = 0; i < five; i++) {
+          answer += "V";
+        }
+
+        for (int i = 0; i < one; i++) {
+            answer += "I";
+        }
+
+        System.out.println(answer);
+    }
+}
