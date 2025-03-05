6f43ab74c781fdab4ee5ad4bded1f27a72e0088b
ohjuyoungg <ejrm12@naver.com>
2025-03-05 16:34:54 +0900

    백준: 火曜日 (Tuesday) (250305)

---
+++ study/src/juyoungoh/beakjoon/_31611.java
@@ -0,0 +1,20 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _31611 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int num = n % 7;
+        if (num == 2) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
