d7f2f02a61b1607422fdd9d501dff50dcc6654d5
ohjuyoungg <ejrm12@naver.com>
2025-03-06 14:58:33 +0900

    백준: 三方比較 (Three-Way Comparison) (250306)

---
+++ study/src/juyoungoh/beakjoon/_27328.java
@@ -0,0 +1,23 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _27328 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        if (a < b) {
+            System.out.println(-1);
+        } else if (a == b) {
+            System.out.println(0);
+        } else {
+            System.out.println(1);
+        }
+    }
+}
