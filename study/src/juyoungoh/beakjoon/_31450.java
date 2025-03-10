9a3d5117704cf714a4e0bbf55e3719e4f00a8d4d
ohjuyoungg <ejrm12@naver.com>
2025-03-10 21:27:25 +0900

    백준: Everyone is a winner (250310)

---
+++ study/src/juyoungoh/beakjoon/_31450.java
@@ -0,0 +1,21 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _31450 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s[] = br.readLine().split( " ");
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
+        if (a % b == 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
