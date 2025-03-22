e9928c0480d1b87c8e7424d11a3f7506e97a40fe
ohjuyoungg <ejrm12@naver.com>
2025-03-22 13:14:22 +0900

    백준: Just Round Down (250322)

--- study/src/juyoungoh/beakjoon/_32458.java
+++ study/src/juyoungoh/beakjoon/_32458.java
@@ -8,6 +8,7 @@ public class _32458 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         double n = Double.parseDouble(br.readLine());
         int answer = (int) n;
         System.out.println(answer);
