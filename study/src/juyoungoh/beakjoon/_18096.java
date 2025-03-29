35110508fd5eb579eb29facfbb00788f4a5cabc4
ohjuyoungg <ejrm12@naver.com>
2025-03-29 16:36:33 +0900

    백준: Арифметическая магия (250329)

--- study/src/juyoungoh/beakjoon/_18096.java
+++ study/src/juyoungoh/beakjoon/_18096.java
@@ -8,7 +8,6 @@ public class _18096 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
         System.out.println(1);
     }
