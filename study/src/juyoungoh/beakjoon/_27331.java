897a09424c25b065d04a2baa835f575cae7d3600
ohjuyoungg <ejrm12@naver.com>
2025-04-02 21:37:53 +0900

    백준: 2 桁の整数 (Two-digit Integer) (250402)

--- study/src/juyoungoh/beakjoon/_27331.java
+++ study/src/juyoungoh/beakjoon/_27331.java
@@ -8,7 +8,6 @@ public class _27331 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String s = br.readLine();
         String s2 = br.readLine();
 
--- study/src/juyoungoh/beakjoon/_8871.java
+++ study/src/juyoungoh/beakjoon/_8871.java
@@ -8,7 +8,6 @@ public class _8871 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
 
         int min = 2 + (3 * n);
