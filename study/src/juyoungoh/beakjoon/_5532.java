352a4f748aa880c83e1f5dda52f6130e83dcb609
ohjuyoungg <ejrm12@naver.com>
2025-04-04 21:16:14 +0900

    백준: 방학 숙제 (250404)

--- study/src/juyoungoh/beakjoon/_5532.java
+++ study/src/juyoungoh/beakjoon/_5532.java
@@ -8,7 +8,6 @@ public class _5532 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int day = Integer.parseInt(br.readLine());
         int a = Integer.parseInt(br.readLine());
         int b = Integer.parseInt(br.readLine());
