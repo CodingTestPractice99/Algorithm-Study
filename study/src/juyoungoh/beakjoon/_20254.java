b037be4820cc12dadaf4332c5ea4f763e0ae9dce
ohjuyoungg <ejrm12@naver.com>
2025-04-07 21:17:09 +0900

    백준: Site Score (250407)

--- study/src/juyoungoh/beakjoon/_20254.java
+++ study/src/juyoungoh/beakjoon/_20254.java
@@ -8,7 +8,6 @@ public class _20254 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String s[] = br.readLine().split(" ");
 
         int a = Integer.parseInt(s[0]) * 56;
