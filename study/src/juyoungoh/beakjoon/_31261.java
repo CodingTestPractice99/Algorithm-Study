670a57ff88804c405acf606a13f6fda7bdc821b4
ohjuyoungg <ejrm12@naver.com>
2025-04-08 19:54:15 +0900

    백준: НАМИСЛИХ СИ ЧИСЛО (250408)

---
+++ study/src/juyoungoh/beakjoon/_31261.java
@@ -0,0 +1,21 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _31261 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s[] = br.readLine().split(" ");
+
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
+
+        long answer = a * (a * (b + a) + a);
+        System.out.println(answer);
+
+    }
+}
