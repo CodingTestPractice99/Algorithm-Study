3b949d2863b23594e0272605a0fec8d953bea96e
ohjuyoungg <ejrm12@naver.com>
2025-04-09 16:32:23 +0900

    백준: Cutting Corners (250409)

---
+++ study/src/juyoungoh/beakjoon/_20215.java
@@ -0,0 +1,19 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _20215 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] s = br.readLine().split(" ");
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
+
+        double result = a + b - Math.sqrt(a * a + b * b);
+        System.out.printf("%.9f\n", result);
+    }
+}
