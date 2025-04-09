1404317816756af81e2d01d500f9f70711adec71
ohjuyoungg <ejrm12@naver.com>
2025-04-09 16:16:10 +0900

    백준: Kuber (250409)

--- study/src/juyoungoh/beakjoon/_20833.java
+++ study/src/juyoungoh/beakjoon/_20833.java
@@ -8,7 +8,6 @@ public class _20833 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
 
         double answer = 0;
