9e24ed2d425dc905c9fe1ebecac8c4c1eef13126
ohjuyoungg <ejrm12@naver.com>
2025-04-09 16:14:40 +0900

    백준: Piece of Cake! (250409)

---
+++ study/src/juyoungoh/beakjoon/_17874.java
@@ -0,0 +1,26 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _17874 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s[] = br.readLine().split(" ");
+
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
+        int c = Integer.parseInt(s[2]);
+
+        int b2 = a - b;
+        int c2 = a - c;
+
+        int max1 = Math.max(b, b2);
+        int max2 = Math.max(c, c2);
+
+        System.out.println(max1 * max2 * 4);
+    }
+}
