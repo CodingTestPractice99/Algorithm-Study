1fd0b2e79c7a17d80eb8354f1b89e35e0bccd94c
ohjuyoungg <ejrm12@naver.com>
2025-04-12 21:29:13 +0900

    백준: Easy-to-Solve Expressions (250412)

--- study/src/juyoungoh/beakjoon/_25784.java
+++ study/src/juyoungoh/beakjoon/_25784.java
@@ -9,7 +9,6 @@ public class _25784 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String s[] = br.readLine().split(" ");
 
         int a = Integer.parseInt(s[0]);
