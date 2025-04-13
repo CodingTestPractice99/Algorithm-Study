5d95a6009327b2fa3aee28165ec2eae24ecb6efd
ohjuyoungg <ejrm12@naver.com>
2025-04-13 13:55:48 +0900

    백준: ハミング距離 (Hamming Distance) (250413)

--- study/src/juyoungoh/beakjoon/_31668.java
+++ study/src/juyoungoh/beakjoon/_31668.java
@@ -4,22 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class _31608 {
+public class _31668 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
 
-        String str1[] = br.readLine().split("");
-        String str2[] = br.readLine().split("");
-
-        int answer = 0;
-        for (int i = 0; i < str1.length; i++) {
-            if (!(str1[i].equals(str2[i]))) {
-                answer++;
-            }
-        }
-        System.out.println(answer);
+        System.out.println((m / n) * k);
     }
 }
