b191debf7cc3ab76f8a832887671ff92f4db0b16
ohjuyoungg <ejrm12@naver.com>
2025-04-13 13:43:26 +0900

    백준: 특별한 가지 (250413)

--- study/src/juyoungoh/beakjoon/_31608.java
+++ study/src/juyoungoh/beakjoon/_31608.java
@@ -1,5 +1,18 @@
 package juyoungoh.beakjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class _31608 {
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+
+        System.out.println((m / n) * k);
+    }
 }
