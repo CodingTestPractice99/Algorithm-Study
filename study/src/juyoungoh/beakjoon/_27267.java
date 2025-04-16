3f3baa69f3df1e9d2a6e7272157bee5339cf7484
ohjuyoungg <ejrm12@naver.com>
2025-04-16 16:34:30 +0900

    백준: Сравнение комнат (250416)

---
+++ study/src/juyoungoh/beakjoon/_27267.java
@@ -0,0 +1,25 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _27267 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s[] = br.readLine().split(" ");
+
+        int m = Integer.parseInt(s[0]) * Integer.parseInt(s[1]);
+        int p = Integer.parseInt(s[2]) * Integer.parseInt(s[3]);
+
+        if (m > p) {
+            System.out.println("M");
+        } else if (m < p) {
+            System.out.println("P");
+        } else {
+            System.out.println("E");
+        }
+    }
+}
