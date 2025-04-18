679e5147019ee33eefb15fdcf219620935cd8678
ohjuyoungg <ejrm12@naver.com>
2025-04-18 20:44:07 +0900

    백준: Сравнение комнат (250418)

--- study/src/juyoungoh/beakjoon/_27267.java
+++ study/src/juyoungoh/beakjoon/_27267.java
@@ -8,7 +8,6 @@ public class _27267 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String s[] = br.readLine().split(" ");
 
         int m = Integer.parseInt(s[0]) * Integer.parseInt(s[1]);
