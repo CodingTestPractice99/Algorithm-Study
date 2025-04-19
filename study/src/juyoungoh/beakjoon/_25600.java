d3f9f8d2481b72a1316f7eab9f95c197db2512d2
ohjuyoungg <ejrm12@naver.com>
2025-04-19 20:22:48 +0900

    백준: Triathlon (250419)

--- study/src/juyoungoh/beakjoon/_25600.java
+++ study/src/juyoungoh/beakjoon/_25600.java
@@ -8,7 +8,6 @@ public class _25600 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
         int max = 0;
 
