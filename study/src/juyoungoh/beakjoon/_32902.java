2193fe435849d473e5ff190fc3fb1e39eb1ddde3
ohjuyoungg <ejrm12@naver.com>
2025-04-23 16:30:16 +0900

    백준: Chips (250423)

---
+++ study/src/juyoungoh/beakjoon/_32902.java
@@ -0,0 +1,21 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _32902 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s[] = br.readLine().split(" ");
+
+        int k = Integer.parseInt(s[0]);
+        int n = Integer.parseInt(s[1]);
+
+        int min = n + 1;
+        int max = k * n + 1;
+        System.out.println(min + " " + max);
+    }
+}
