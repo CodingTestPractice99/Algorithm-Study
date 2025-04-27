30c63ebab7746a716c114977e1f4d67bdfd39490
ohjuyoungg <ejrm12@naver.com>
2025-04-27 13:01:54 +0900

    백준: Skarpetki (250427)

---
+++ study/src/juyoungoh/beakjoon/_26742.java
@@ -0,0 +1,29 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _26742 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s[] = br.readLine().split("");
+        int b = 0;
+        int c = 0;
+
+        for (int i = 0; i < s.length; i++) {
+            if (s[i].equals("B")) {
+                b++;
+            } else if (s[i].equals("C")) {
+                c++;
+            }
+        }
+
+        int b2 = b / 2;
+        int c2 = c / 2;
+
+        System.out.println(b2 + c2);
+    }
+}
