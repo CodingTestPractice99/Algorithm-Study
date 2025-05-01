7a0ceb65addb4afa9444cb9d19b91fb6f924f4ad
ohjuyoungg <ejrm12@naver.com>
2025-05-01 14:19:01 +0900

    백준: Divide the Cash (250501)

--- study/src/juyoungoh/beakjoon/_25858.java
+++ study/src/juyoungoh/beakjoon/_25858.java
@@ -10,7 +10,6 @@ public class _25858 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String s[] = br.readLine().split(" ");
 
         int n = Integer.parseInt(s[0]);
