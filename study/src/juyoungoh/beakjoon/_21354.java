e7951b832bd4056d21d6d54d75ea2bf8e6c3aa26
ohjuyoungg <ejrm12@naver.com>
2025-05-15 12:38:48 +0900

    백준: Äpplen och päron (250515)

---
+++ study/src/juyoungoh/beakjoon/_21354.java
@@ -0,0 +1,25 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _21354 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s[] = br.readLine().split(" ");
+
+        int a = Integer.parseInt(s[0]) * 7;
+        int b = Integer.parseInt(s[1]) * 13;
+
+        if (a < b) {
+            System.out.println("Petra");
+        } else if (a > b) {
+            System.out.println("Axel");
+        } else {
+            System.out.println("lika");
+        }
+    }
+}
