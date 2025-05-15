f4b0f16d6d3ba4455618c81ab34db0f0217675b0
ohjuyoungg <ejrm12@naver.com>
2025-05-15 12:23:56 +0900

    백준: 試験 (Exam) (250515)

---
+++ study/src/juyoungoh/beakjoon/_18411.java
@@ -0,0 +1,25 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class _18411 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s[] = br.readLine().split(" ");
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < s.length; i++) {
+            list.add(Integer.parseInt(s[i]));
+        }
+
+        list.sort(null);
+
+        System.out.println(list.get(1) + list.get(2));
+    }
+}
