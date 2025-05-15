280bb49942be106c3f3aee652630318e32031180
ohjuyoungg <ejrm12@naver.com>
2025-05-15 12:30:08 +0900

    백준: Previous Level (250515)

---
+++ study/src/juyoungoh/beakjoon/_28453.java
@@ -0,0 +1,31 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _28453 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        String s[] = br.readLine().split(" ");
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length; i++) {
+            if (Integer.parseInt(s[i]) == 300) {
+                sb.append(1).append(" ");
+            } else if (Integer.parseInt(s[i]) >= 275) {
+                sb.append(2).append(" ");
+            } else if (Integer.parseInt(s[i]) >= 250) {
+                sb.append(3).append(" ");
+            } else if (Integer.parseInt(s[i]) < 250) {
+                sb.append(4).append(" ");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
+
