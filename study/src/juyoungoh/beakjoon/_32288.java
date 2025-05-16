f8ae1488b11f2c22584ff0fc26dfd583984b4c4e
ohjuyoungg <ejrm12@naver.com>
2025-05-16 15:52:18 +0900

    백준: 바코드 닉네임 (250516)

--- study/src/juyoungoh/beakjoon/_32288.java
+++ study/src/juyoungoh/beakjoon/_32288.java
@@ -9,7 +9,6 @@ public class _32288 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
         String s[] = br.readLine().split("");
 
         String str = "";
