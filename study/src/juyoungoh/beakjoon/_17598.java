6f80348badafdf093b1e9a732202f3ae0b5ae39d
ohjuyoungg <ejrm12@naver.com>
2025-05-24 08:27:40 +0900

    백준: Animal King Election (250524)

--- study/src/juyoungoh/beakjoon/_17598.java
+++ study/src/juyoungoh/beakjoon/_17598.java
@@ -8,7 +8,6 @@ public class _17598 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int lion = 0;
         int tiger = 0;
 
