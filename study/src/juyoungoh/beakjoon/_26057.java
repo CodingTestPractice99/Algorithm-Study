38a343e6c23ccb251ec0ba811a1316759ee4749a
ohjuyoungg <ejrm12@naver.com>
2025-06-01 19:01:10 +0900

    백준: Большой удой (250601)

--- study/src/juyoungoh/beakjoon/_26057.java
+++ study/src/juyoungoh/beakjoon/_26057.java
@@ -8,7 +8,6 @@ public class _26057 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
 
