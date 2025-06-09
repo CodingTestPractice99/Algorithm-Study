28d52825cbf6d131af34419eb02efe9237e32b0a
ohjuyoungg <ejrm12@naver.com>
2025-06-09 11:25:37 +0900

    백준: Circus (250609)

---
+++ study/src/juyoungoh/beakjoon/_20352.java
@@ -0,0 +1,18 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _20352 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Long n = Long.parseLong(br.readLine());
+
+        double c = 2 * Math.sqrt(n * Math.PI);
+        System.out.printf("%.10f\n", c);
+
+    }
+}
