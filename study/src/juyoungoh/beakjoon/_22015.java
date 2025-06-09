093bdc89464ec9cf3b285f1192a4244bd387e297
ohjuyoungg <ejrm12@naver.com>
2025-06-09 11:39:01 +0900

    백준: 金平糖 (Konpeito) (250609)

---
+++ study/src/juyoungoh/beakjoon/_22015.java
@@ -0,0 +1,23 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _22015 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s[] = br.readLine().split(" ");
+
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+        int z = Integer.parseInt(s[2]);
+
+        int max_val = Math.max(n, Math.max(m, z));
+
+        int result = (max_val - n) + (max_val - m) + (max_val - z);
+        System.out.println(result);
+    }
+}
