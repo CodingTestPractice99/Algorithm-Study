cdaf096db52aa53ec7e00bb03962f608ce571f79
ohjuyoungg <ejrm12@naver.com>
2025-06-10 09:10:32 +0900

    백준: SciComLove (2024) (250610)

---
+++ study/src/juyoungoh/beakjoon/_31746.java
@@ -0,0 +1,23 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _31746 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        String s = "SciComLove";
+
+        if (n % 2 == 0) {
+            System.out.println(s);
+        } else {
+            String reversed = new StringBuilder(s).reverse().toString();
+            System.out.println(reversed);
+        }
+    }
+}
