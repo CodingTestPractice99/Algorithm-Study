63c79bbfc3f15bc0f5c4ae86dbbabdf22cea0729
ohjuyoungg <ejrm12@naver.com>
2025-06-19 20:39:33 +0900

    백준: Christmas Tree Adapter (250619)

--- study/src/juyoungoh/beakjoon/_32314.java
+++ study/src/juyoungoh/beakjoon/_32314.java
@@ -8,7 +8,6 @@ public class _32314 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
         String s[] = br.readLine().split(" ");
 
