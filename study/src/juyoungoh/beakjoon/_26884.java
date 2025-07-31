5326a7b195609ccfb2582e446416f390d0d49ea0
ohjuyoungg <ejrm12@naver.com>
2025-07-31 14:17:05 +0900

    백준: Kent-buggen (250731)

---
+++ study/src/juyoungoh/beakjoon/_26884.java
@@ -0,0 +1,31 @@
+package juyoungoh.beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class _26884 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        int answer = 0;
+        for (Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1) {
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
