ad32e95b9710ff8227798f8cf4df78b07f56da8d
ohjuyoungg <ejrm12@naver.com>
2025-08-16 21:53:18 +0900

    백준: Betting (250816)

--- study/src/juyoungoh/beakjoon/_24751.java
+++ study/src/juyoungoh/beakjoon/_24751.java
@@ -8,7 +8,6 @@ public class _24751 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
         int m = 100 - n;
 
